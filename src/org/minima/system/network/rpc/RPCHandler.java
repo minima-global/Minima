d612ad0073e88f9e7d0af3fbb94c6e2d356039e9
spartacus <spartacus@localhost>
2020-02-12 12:01:47 +0000

    don't lowercase the RPC call

--- src/org/minima/system/network/rpc/RPCHandler.java
+++ src/org/minima/system/network/rpc/RPCHandler.java
@@ -67,7 +67,7 @@ public class RPCHandler implements Runnable {
 			String method = parse.nextToken().toUpperCase(); // we get the HTTP method of the client
 			
 			// we get file requested
-			fileRequested = parse.nextToken().toLowerCase();
+			fileRequested = parse.nextToken();
 			
 			// we support only GET and HEAD methods, we check
 			if (method.equals("GET")){
