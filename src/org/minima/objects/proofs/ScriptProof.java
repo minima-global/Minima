041aae4f38c265bf4f4ce33e3976dac43a7aad8e
spartacusrex <patrickcerri@gmail.com>
2020-03-19 07:25:42 +0000

    New ScriptProof Object

--- src/org/minima/objects/proofs/ScriptProof.java
+++ src/org/minima/objects/proofs/ScriptProof.java
@@ -1,15 +1,68 @@
 package org.minima.objects.proofs;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import org.minima.objects.base.MiniHash;
 import org.minima.objects.base.MiniString;
 import org.minima.utils.Proof;
+import org.minima.utils.Streamable;
+import org.minima.utils.json.JSONObject;
 
-public class ScriptProof {
+public class ScriptProof implements Streamable {
 
 	MiniString mScript;
 	Proof mProof;
 	
+	private ScriptProof() {}
+	
 	public ScriptProof(MiniString zScript, Proof zProof) {
 		mScript = zScript;
 		mProof  = zProof;
+	}
+	
+	public MiniString getScript() {
+		return mScript;
+	}
+	
+	public Proof getProof() {
+		return mProof;
+	}
+	
+	public MiniHash getProofHash() {
+		return mProof.calculateFinalHash();
+	}
+
+	public JSONObject toJSON() {
+		JSONObject json = new JSONObject();
+		json.put("script", mScript.toString());
+		json.put("proof", mProof.toJSON());
+		return json;
+	}
+	
+	@Override
+	public void writeDataStream(DataOutputStream zOut) throws IOException {
+		mScript.writeDataStream(zOut);
+		mProof.writeDataStream(zOut);
+	}
+
+	@Override
+	public void readDataStream(DataInputStream zIn) throws IOException {
+		mScript = MiniString.ReadFromStream(zIn);
+		mProof  = Proof.ReadFromStream(zIn);
+	}
+	
+	public static ScriptProof ReadFromStream(DataInputStream zIn){
+		ScriptProof proof = new ScriptProof();
+		
+		try {
+			proof.readDataStream(zIn);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return proof;
 	}
 }
--- src/org/minima/utils/Proof.java
+++ src/org/minima/utils/Proof.java
@@ -1,5 +1,6 @@
 package org.minima.utils;
 
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -9,6 +10,8 @@ import org.minima.database.mmr.MMRProof;
 import org.minima.objects.base.MiniByte;
 import org.minima.objects.base.MiniData;
 import org.minima.objects.base.MiniHash;
+import org.minima.utils.json.JSONArray;
+import org.minima.utils.json.JSONObject;
 
 public class Proof implements Streamable {
 
@@ -35,11 +38,17 @@ public class Proof implements Streamable {
 	//The Merkle Branch that when applied to the data gives the final proof;
 	ArrayList<ProofChunk> mProofChain;
 	
+	//Calculate this once
+	MiniHash mFinalHash;
+	MiniData mChainSHA;
+	boolean mFinalized;
+		
 	private Proof(){}
 	
 	public Proof(MiniHash zData) {
-		mData   = zData;
+		mData       = zData;
 		mProofChain = new ArrayList<>();
+		mFinalized  = false;
 	}
 	
 	public void addProofChunk(MiniByte zLeft, MiniHash zHash) {
@@ -50,7 +59,46 @@ public class Proof implements Streamable {
 		return mProofChain.size();
 	}
 	
+	public void finalizeHash() {
+		//Reset so that can be recalculated
+		mFinalized = false;
+		
+		//Recalculate
+		mFinalHash = calculateFinalHash();
+		mChainSHA  = getChainSHAProof();
+		
+		//Ok - it's done now..
+		mFinalized = true;
+	}
+	
+	public MiniData getChainSHAProof() {
+		if(mFinalized) {
+			return mChainSHA;
+		}
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		DataOutputStream dos = new DataOutputStream(baos);
+		
+		try {
+			int len = mProofChain.size();
+			for(int i=0;i<len;i++){
+				ProofChunk chunk = mProofChain.get(i);
+				chunk.getLeft().writeDataStream(dos);
+				chunk.getHash().writeDataStream(dos);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//Convert to MiniData..
+		return new MiniData(baos.toByteArray());
+	}
+	
 	public MiniHash calculateFinalHash() {
+		if(mFinalized) {
+			return mFinalHash;
+		}
+		
 		//Get the Final Hash of the Data
 		MiniHash current = mData;
 		
@@ -68,6 +116,27 @@ public class Proof implements Streamable {
 		return current;
 	}
 
+	public JSONObject toJSON() {
+		JSONObject json = new JSONObject();
+		
+		JSONArray proof = new JSONArray();
+		int len = mProofChain.size();
+		for(int i=0;i<len;i++){
+			JSONObject jsonchunk = new JSONObject();
+			
+			ProofChunk chunk = mProofChain.get(i);
+			jsonchunk.put("left", chunk.getLeft().isTrue());
+			jsonchunk.put("hash", chunk.getHash().to0xString());
+			proof.add(chunk);
+		}
+		
+		json.put("proofchain", proof);
+		json.put("chainsha", getChainSHAProof().to0xString());
+		json.put("finalhash", calculateFinalHash().to0xString());
+		
+		return json;
+	}
+	
 	@Override
 	public void writeDataStream(DataOutputStream zOut) throws IOException {
 		mData.writeDataStream(zOut);
@@ -91,6 +160,8 @@ public class Proof implements Streamable {
 			MiniHash hash = MiniHash.ReadFromStream(zIn);
 			mProofChain.add(new ProofChunk(left, hash));
 		}
+		
+		finalizeHash();
 	}
 	
 	public static Proof ReadFromStream(DataInputStream zIn){
@@ -99,9 +170,7 @@ public class Proof implements Streamable {
 		try {
 			proof.readDataStream(zIn);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return null;
 		}
 		
 		return proof;
