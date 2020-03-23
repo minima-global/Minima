6cc7993737fe19bfbba4bc2daa15af7699d6e935
spartacusrex <patrickcerri@gmail.com>
2020-03-23 08:26:29 +0000

    Help fixes

--- src/org/minima/system/input/functions/help.java
+++ src/org/minima/system/input/functions/help.java
@@ -59,12 +59,13 @@ public class help extends CommandFunction{
 			
 			addJSONDesc(new status());
 			addJSONDesc(new history());
+			addJSONDesc(new backup());
+			
 			addJSONDesc(new printchain());
 			addJSONDesc(new printtree());
 	
-			addJSONDesc(new trace());
-			addJSONDesc(new minetrans());
-			addJSONDesc(new backup());
+//			addJSONDesc(new trace());
+//			addJSONDesc(new minetrans());
 			
 			addJSONDesc(new connect());
 			addJSONDesc(new disconnect());
@@ -73,6 +74,7 @@ public class help extends CommandFunction{
 			
 			addJSONDesc(new gimme50());
 			addJSONDesc(new send());
+			addJSONDesc(new newaddress());
 			addJSONDesc(new balance());
 			
 			addJSONDesc(new keys());
@@ -88,7 +90,6 @@ public class help extends CommandFunction{
 			addJSONDesc(new txpowinfo());
 			
 			addJSONDesc(new addresses());
-			addJSONDesc(new newaddress());
 			addJSONDesc(new newscript());
 			addJSONDesc(new cleanscript());
 			addJSONDesc(new runscript());
@@ -129,6 +130,10 @@ public class help extends CommandFunction{
 		}else {
 			getResponseStream().getDataJSON().put(name, params+ " - " + zFunc.getSimple());
 		}
+	}
+	
+	public void addBlankLine() {
+		getResponseStream().getDataJSON().put("---", "");
 	}
 	
 	public String getStrOfLength(int zDesiredLen, String zString) {
