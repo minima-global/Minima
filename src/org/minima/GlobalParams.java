810fab4a83400452432b47c853e05a9ecf11a071
spartacusrex <patrickcerri@gmail.com>
2020-04-05 15:45:14 +0100

    Latest JAR 0.82

--- src/org/minima/GlobalParams.java
+++ src/org/minima/GlobalParams.java
@@ -7,7 +7,7 @@ public class GlobalParams {
 	/**
 	 * Which Version
 	 */
-	public static final String MINIMA_VERSION = "0.81";
+	public static final String MINIMA_VERSION = "0.82";
 	
 	/**
 	 * Number of seconds before sending a pulse message
