d2f09e1be75cdbeac7e86672b53253672a83ed0c
spartacusrex <patrickcerri@gmail.com>
2020-04-28 11:44:54 +0100

    break up big files..

--- src/org/minima/utils/FileUtil.java
+++ src/org/minima/utils/FileUtil.java
@@ -4,9 +4,14 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
+
+import org.minima.objects.base.MiniData;
 
 public class FileUtil {
 
+	public final static int MAX_STRING = 10000;
+	
 	public static void ConvertFileToHex(String zFilePath) {
 		File ff = new File(zFilePath);
     	
@@ -26,8 +31,31 @@ public class FileUtil {
 			e.printStackTrace();
 		} 
 		
-    	//And Now convert to HEX!
-    	System.out.println(BaseConverter.encode16(ret));
+	    //Now chop it up..
+    	System.out.println("//FILE SIZE "+size);
+		if(size>MAX_STRING) {
+    		int num = (int) (size / MAX_STRING) + 1;
+    		System.out.println("//HEX NUM "+num);
+    		for(int i=0;i<num;i++) {
+    			//loop through it..
+    			int start = i*MAX_STRING;
+    			int end   = ((i+1)*MAX_STRING)-1;
+    			
+    			if(end > size) {
+    				end = (int) size;
+    			}
+    			
+    			System.out.println("//"+start+" - "+end);
+    			byte[] copy = Arrays.copyOfRange(ret, start, end);
+    			String output = "public final static byte[] HEXDATA"+i+" = new MiniData(\""+BaseConverter.encode16(copy)+"\").getData();";
+            	System.out.println(output);
+    		}
+    		
+    	}else {
+    		String output = "public final static byte[] HEXDATA = new MiniData(\""+BaseConverter.encode16(ret)+"\").getData();";
+        	System.out.println(output);	
+    	}
+    	
 	}
 	
 	public static void main(String[] zArgs) {
