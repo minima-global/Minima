72c8bef98e4cb0e3d8856b38f9453e2c6c4669e2
spartacusrex <patrickcerri@gmail.com>
2020-05-04 17:20:57 +0100

    0.87.24

--- src/org/minima/GlobalParams.java
+++ src/org/minima/GlobalParams.java
@@ -7,7 +7,7 @@ public class GlobalParams {
 	/**
 	 * Which Version
 	 */
-	public static final String MINIMA_VERSION = "0.87.23";
+	public static final String MINIMA_VERSION = "0.87.24";
 	
 	/**
 	 * Number of seconds before sending a pulse message
@@ -24,7 +24,7 @@ public class GlobalParams {
 	/**
 	 * Speed in blocks per second
 	 */
-	public static final MiniNumber MINIMA_BLOCK_SPEED  = new MiniNumber("0.5");
+	public static final MiniNumber MINIMA_BLOCK_SPEED  = new MiniNumber("0.05");
 	
 	/**
 	 * How deep before we think confirmed..
@@ -34,7 +34,7 @@ public class GlobalParams {
 	/**
 	 * Depth before we cascade..
 	 */
-	public static final int MINIMA_CASCADE_START_DEPTH   = 128;
+	public static final int MINIMA_CASCADE_START_DEPTH   = 1024;
 	
 	/**
 	 * Minimum number of blocks at each cascade level 
