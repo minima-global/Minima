84df798c34985161c379ddfe5c11baf532bdff4a
spartacusrex <patrickcerri@gmail.com>
2020-05-09 19:35:19 +0100

    added unkeepcoin to help..

--- src/org/minima/system/input/functions/help.java
+++ src/org/minima/system/input/functions/help.java
@@ -92,7 +92,7 @@ public class help extends CommandFunction{
 			addJSONDesc(new exportcoin());
 			addJSONDesc(new importcoin());
 			addJSONDesc(new keepcoin());
-//			addJSONDesc(new unkeepcoin());
+			addJSONDesc(new unkeepcoin());
 			
 //			addJSONDesc(new search());
 			addJSONDesc(new txpowsearch());
