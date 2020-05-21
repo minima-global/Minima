bb2036baf5457940bbb795548dd9b0518faf70fa
spartacusrex <patrickcerri@gmail.com>
2020-05-21 08:59:41 +0100

    Fixed up the new minima.js with SQL as a static java class

--- src/org/minima/GlobalParams.java
+++ src/org/minima/GlobalParams.java
@@ -7,7 +7,7 @@ public class GlobalParams {
 	/**
 	 * Which Version
 	 */
-	public static final String MINIMA_VERSION = "0.91.1"; 
+	public static final String MINIMA_VERSION = "0.91.2"; 
 	
 	/**
 	 * Number of seconds before sending a pulse message
