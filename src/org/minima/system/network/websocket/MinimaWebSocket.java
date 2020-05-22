d59860b576c8cb14cd930796654f1534105429bc
spartacusrex <patrickcerri@gmail.com>
2020-05-22 10:57:40 +0100

    all messages sent to wsmanager

--- src/org/minima/system/network/websocket/MinimaWebSocket.java
+++ src/org/minima/system/network/websocket/MinimaWebSocket.java
@@ -22,29 +22,38 @@ public class MinimaWebSocket extends WebSocket {
 	@Override
 	protected void onOpen() {
 		//Tell the manager
-		Message newclient = new Message(WebSocketManager.WEBSOCK_ONOPEN);
-		newclient.addObject("wsclient", this);
-		mManager.PostMessage(newclient);	
+		Message msg = new Message(WebSocketManager.WEBSOCK_ONOPEN);
+		msg.addObject("wsclient", this);
+		mManager.PostMessage(msg);	
 	}
 
 	@Override
 	protected void onClose(CloseCode code, String reason, boolean initiatedByRemote) {
-		
+		//Tell the manager
+		Message msg = new Message(WebSocketManager.WEBSOCK_ONCLOSE);
+		msg.addObject("wsclient", this);
+		mManager.PostMessage(msg);
 	}
 
 	@Override
 	protected void onMessage(WebSocketFrame message) {
-		
+		//Tell the manager
+		Message msg = new Message(WebSocketManager.WEBSOCK_ONMESSAGE);
+		msg.addObject("wsclient", this);
+		msg.addString("message", message.getTextPayload());
+		mManager.PostMessage(msg);
 	}
 
 	@Override
-	protected void onPong(WebSocketFrame pong) {
-		
-	}
+	protected void onPong(WebSocketFrame pong) {}
 
 	@Override
 	protected void onException(IOException exception) {
-		
+		//Tell the manager
+		Message msg = new Message(WebSocketManager.WEBSOCK_ONMESSAGE);
+		msg.addObject("wsclient", this);
+		msg.addString("exception", exception.toString());
+		mManager.PostMessage(msg);
 	}
 
 }
