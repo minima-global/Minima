80ba6abc182068356858d518e8ac2a3f7ccd9aab
Jerome Rousselot <jerome@jita.digital>
2020-06-16 12:53:54 +0100

    MiniIntegerTests: added divRoundDown,  readStreamTooLarge, string constructor.

--- test/org/minima/tests/objects/base/MiniIntegerTests.java
+++ test/org/minima/tests/objects/base/MiniIntegerTests.java
@@ -21,7 +21,7 @@ public class MiniIntegerTests {
     
     @Test
     public void testValueZero() {
-      final MiniInteger i = new MiniInteger(0);
+      MiniInteger i = new MiniInteger(0);
       assertNotNull("should not be null", i);
       assertTrue("should be equal to constant zero", i.isEqual(MiniInteger.ZERO));
       assertFalse("should not be equal to constant one", i.isEqual(MiniInteger.ONE));
@@ -41,15 +41,59 @@ public class MiniIntegerTests {
         DataInputStream dis = new DataInputStream(inputStream);
 
         i.readDataStream(dis);
-        System.out.println(" i is now equal to " + i.toString());
+        assertTrue("should be equal to one", i.isEqual(MiniInteger.ONE));
+        dis.reset();
+        i = MiniInteger.ReadFromStream(dis);
         assertTrue("should be equal to one", i.isEqual(MiniInteger.ONE));
       } catch(final IOException e) {
         System.out.println("IOException: " + e.toString() + " msg=" + e.getMessage());
         assertTrue(" there should not be an IOException", false);
       }
+      i = new MiniInteger("0");
+      assertTrue(" read string 0 return mini integer 0", i.isEqual(MiniInteger.ZERO));
+      MiniInteger minus1 = i.sub(MiniInteger.ONE);
+      assertTrue(" 0 - 1 should equal -1", minus1.isEqual(new MiniInteger(-1)));
+
     }
 
+    @Test 
+    public void testReadStreamTooLarge() {
+      try {
+        MiniInteger i = new MiniInteger(0);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(bos);
+        //MiniInteger.ONE.writeDataStream(dos);
+
+        for(int j=0; j < 1000; j++) {
+          bos.write(127); // large possible value, MSB is +/- sign
+        }
+
+        InputStream inputStream = new ByteArrayInputStream(bos.toByteArray());
+        DataInputStream dis = new DataInputStream(inputStream);
+
+        i.readDataStream(dis);
+        System.out.println(" i is now equal to " + i.toString());
+        assertFalse("This line should not be reached ", true);
+      } catch(final IOException e) {
+        System.out.println("IOException: " + e.toString() + " msg=" + e.getMessage());
+        assertTrue(" there should be an IOException with message input too large ", 
+                    e.getMessage().contains(new String("input too large")));
+      }
+     
+    }
+
+    @Test
+    public void testDivRoundDown() {
+      MiniInteger three = new MiniInteger(3);
+      MiniInteger result = three.divRoundDown(MiniInteger.TWO);
+      assertTrue(" 3 / 2 should be equal to 1", result.isEqual(MiniInteger.ONE));
+      result = MiniInteger.TWO.divRoundDown(MiniInteger.TWO);
+      assertTrue(" 2 / 2 should be equal to 1", result.isEqual(MiniInteger.ONE));
+      result = MiniInteger.TWO.divRoundDown(MiniInteger.ONE);
+      assertTrue(" 2 / 1 should be equal to 2", result.isEqual(MiniInteger.TWO));
+      result = MiniInteger.ONE.divRoundDown(MiniInteger.TWO);
+      assertTrue(" 1 / 2 should be equal to 0", result.isEqual(MiniInteger.ZERO));
 
 
-
+    }
 }
