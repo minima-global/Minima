a9d2620ff45f0b2773460e4401fa3bd75cf8d5f2
spartacusrex <patrickcerri@gmail.com>
2020-06-19 13:12:16 +0100

    0.95.3 - MMRSet getEntry recursive fix

--- src/org/minima/GlobalParams.java
+++ src/org/minima/GlobalParams.java
@@ -7,7 +7,7 @@ public class GlobalParams {
 	/**
 	 * Which Version
 	 */
-	public static final String MINIMA_VERSION = "0.95.2"; 
+	public static final String MINIMA_VERSION = "0.95.3"; 
 	
 	/**
 	 * Number of seconds before sending a pulse message
