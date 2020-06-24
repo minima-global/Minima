5fb60968dd2ad5bec166c4247ca99ed4a4ba5740
beshogun <ben.roy@omnitude.tech>
2020-06-24 11:39:52 +0100

    added utils sha256 and keccak, keccak at 92% but needs tidied and expanded

---
+++ test/org/minima/tests/utils/SHA256Tests.java
@@ -0,0 +1,17 @@
+package org.minima.tests.utils;
+
+// package org.bouncycastle.crypto.test;
+
+// import org.bouncycastle.crypto.Digest;
+// import org.bouncycastle.crypto.Mac;
+// import org.bouncycastle.crypto.digests.KeccakDigest;
+// import org.bouncycastle.crypto.macs.HMac;
+// import org.bouncycastle.crypto.params.KeyParameter;
+// import org.bouncycastle.util.Arrays;
+// import org.bouncycastle.util.encoders.Hex;
+// import org.bouncycastle.util.test.SimpleTest;
+
+
+public class SHA256Tests {
+    
+}
