34190b8a00011a97280c22f4e1ad9087921efca3
spartacusrex <patrickcerri@gmail.com>
2020-07-01 09:18:21 +0100

    Main Backend JS class

---
+++ src/org/minima/system/network/minidapps/minilib/BackBoneDAPP.java
@@ -0,0 +1,28 @@
+package org.minima.system.network.minidapps.minilib;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+public class BackBoneDAPP {
+
+	/**
+	 * JavaScript
+	 */
+	String mScript;
+	
+	/**
+	 * The Context it is running in
+	 */
+	Context mContext;
+	
+	/**
+	 * The Script Scope
+	 */
+	Scriptable mScope;
+	
+	public BackBoneDAPP() {
+		
+	}
+	
+	
+}
