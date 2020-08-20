ca8cd46a25c36805de12aee759b966e5a5b3d6ed
spartacusrex <patrickcerri@gmail.com>
2020-08-20 10:10:09 +0100

    16384 blocks pre cascade

--- src/org/minima/GlobalParams.java
+++ src/org/minima/GlobalParams.java
@@ -24,7 +24,7 @@ public class GlobalParams {
 	/**
 	 * Speed in blocks per second.. 0.04 = 25 second block time
 	 */
-	public static final MiniNumber MINIMA_BLOCK_SPEED  = new MiniNumber("0.04");
+	public static final MiniNumber MINIMA_BLOCK_SPEED  = new MiniNumber("0.033");
 	
 	/**
 	 * When checking speed and average difficulty only look at this many blocks back
@@ -40,7 +40,7 @@ public class GlobalParams {
 	/**
 	 * Depth before we cascade..
 	 */
-	public static final MiniNumber MINIMA_CASCADE_START_DEPTH = new MiniNumber(32768);
+	public static final MiniNumber MINIMA_CASCADE_START_DEPTH = new MiniNumber(16384);
 	
 	/**
 	 * Number of blocks at each cascade level 
