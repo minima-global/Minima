82bc35bd287ddbc3cf1b00f674076e634ae85680
spartacusrex <patrickcerri@gmail.com>
2020-08-21 12:38:48 +0100

    new start stop

--- scripts/startminima.sh
+++ scripts/startminima.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-
+#The current time
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 
+#Create a filename
 new_fileName="minilogs_"$current_time.txt
 echo "New Logs FileName: " "$new_fileName"
 
 #Make sure exists
-mkdir -p minilogs
+mkdir -p ~/minilogs
 
 #Run it in the background
-nohup java -jar /home/spartacusrex/git/Minima/jar/minima.jar -clean -private -daemon > ./minilogs/$new_fileName &
+nohup java -jar ~/Minima/jar/minima.jar -clean -private -daemon > ~/minilogs/$new_fileName &
 
 
--- scripts/stopminima.sh
+++ scripts/stopminima.sh
@@ -4,7 +4,7 @@
 curl 127.0.0.1:9002/quit
 
 #Give it a second
-sleep 5
+sleep 3
 
 #Stop using the hard way
 kill $(ps aux | grep minima | awk '{print $2}')
