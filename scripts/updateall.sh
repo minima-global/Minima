1b981413e435c260705901a2266cfd65cfdeb34b
spartacusrex <patrickcerri@gmail.com>
2020-08-21 18:10:01 +0100

    new update all

--- scripts/updateall.sh
+++ scripts/updateall.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-#Run through the servers..
+#First update the main server
+./update.sh minima-private-1-vm private
+
+#Update the miner..
+./update.sh minima-miner-1-vm miner
+
+#Run through the public servers..
 ./update.sh minima-public-1-vm public
 ./update.sh minima-public-2-vm public
 ./update.sh minima-public-3-vm public
 ./update.sh minima-public-4-vm public
 
-#./update.sh minima-private-1-vm private
-#./update.sh minima-miner-1-vm miner
 
