e9a4f7058ef0d23820033fb18fe5fd77be5caa3c
spartacusrex <patrickcerri@gmail.com>
2020-08-21 16:08:22 +0100

    no clean

--- scripts/startminima.sh
+++ scripts/startminima.sh
@@ -11,6 +11,6 @@ echo "New Logs FileName: " "$new_fileName"
 mkdir -p ~/minilogs
 
 #Run it in the background
-nohup java -Xmx6G -jar ~/Minima/jar/minima.jar -daemon -clean -connect 10.164.0.3 9001  > ~/minilogs/$new_fileName &
+nohup java -Xmx6G -jar ~/Minima/jar/minima.jar -daemon -connect 10.164.0.3 9001  > ~/minilogs/$new_fileName &
 
 
