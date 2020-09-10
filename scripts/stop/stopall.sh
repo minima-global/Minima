182c55972c90c07dcb4198ae571647ed0ecc520b
spartacusrex <patrickcerri@gmail.com>
2020-09-10 13:28:07 +0100

    scripts update

--- scripts/stop/stopall.sh
+++ scripts/stop/stopall.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-#./stopnet.sh minima-private-1-vm --zone=europe-west4-a
+./stopnet.sh minima-private-1-vm --zone=europe-west4-a
 ./stopnet.sh minima-private-2-vm --zone=europe-west4-a
 
-#./stopnet.sh minima-miner-1-vm --zone=europe-west2-a
-#./stopnet.sh minima-miner-2-vm --zone=europe-west2-a
-#./stopnet.sh minima-miner-3-vm --zone=europe-west2-a
-#./stopnet.sh minima-miner-4-vm --zone=europe-west2-a
+./stopnet.sh minima-miner-1-vm --zone=europe-west2-a
+./stopnet.sh minima-miner-2-vm --zone=europe-west2-a
+./stopnet.sh minima-miner-3-vm --zone=europe-west2-a
+./stopnet.sh minima-miner-4-vm --zone=europe-west2-a
 
 ./stopnet.sh minima-public-1-vm  --zone=europe-west4-a
 ./stopnet.sh minima-public-2-vm  --zone=europe-west4-a
