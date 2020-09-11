11838dd5b02b796af65853757048734067a4e075
spartacusrex <patrickcerri@gmail.com>
2020-09-11 14:56:23 +0100

    update blanace change

--- src/org/minima/system/brains/ConsensusHandler.java
+++ src/org/minima/system/brains/ConsensusHandler.java
@@ -259,7 +259,6 @@ public class ConsensusHandler extends MessageProcessor {
 				PostDAPPJSONMessage(newblock);
 				
 				//Do the balance.. Update listeners if changed..
-				updateListeners(new Message(CONSENSUS_NOTIFY_BALANCE));
 				PostMessage(new Message(ConsensusPrint.CONSENSUS_BALANCE).addBoolean("hard", true));
 				
 				//Print the tree..
@@ -277,8 +276,8 @@ public class ConsensusHandler extends MessageProcessor {
 				getMainDB().getUserDB().addToHistory(txpow,tokamt);
 				
 				//Do we need to update the balance.. or did we do it already..
+				updateListeners(new Message(CONSENSUS_NOTIFY_BALANCE));
 				if(!newbalance) {
-					updateListeners(new Message(CONSENSUS_NOTIFY_BALANCE));
 					PostMessage(new Message(ConsensusPrint.CONSENSUS_BALANCE).addBoolean("hard", true));
 				}				
 			}
