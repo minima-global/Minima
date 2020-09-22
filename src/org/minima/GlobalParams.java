369e4e12b1d5eb0b35e6687e11a2184b4c092eff
spartacusrex <patrickcerri@gmail.com>
2020-09-22 12:57:42 +0100

    0.96.29

--- src/org/minima/GlobalParams.java
+++ src/org/minima/GlobalParams.java
@@ -7,7 +7,7 @@ public class GlobalParams {
 	/**
 	 * Which Version
 	 */
-	public static final String MINIMA_VERSION = "0.96.28"; 
+	public static final String MINIMA_VERSION = "0.96.29"; 
 	
 	/**
 	 * Number of seconds before sending a pulse message - every 10 minutes
