de91b62330a631ecd6da9a98121274aef39e7c46
S <obucina@jita.digital>
2020-11-21 03:27:32 +0100

    TxPowTree tests added, ~15% coverage

---
+++ test/org/minima/tests/database/txpowtree/BlockTreeTests.java
@@ -0,0 +1,10 @@
+package org.minima.tests.database.txpowtree;
+
+import org.junit.Test;
+
+public class BlockTreeTests {
+
+    @Test
+    public void testConstructors() {
+    }
+}
---
+++ test/org/minima/tests/database/txpowtree/CascadeTreeTests.java
@@ -0,0 +1,10 @@
+package org.minima.tests.database.txpowtree;
+
+import org.junit.Test;
+
+public class CascadeTreeTests {
+
+    @Test
+    public void testConstructors() {
+    }
+}
