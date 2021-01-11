f6e81cdc3a404124de9cd6d81160102d25c389b5
spartacusrex <patrickcerri@gmail.com>
2021-01-11 16:51:02 +0000

    send JSON messages

--- src/org/minima/system/network/maxima/Maxima.java
+++ src/org/minima/system/network/maxima/Maxima.java
@@ -1,6 +1,14 @@
 package org.minima.system.network.maxima;
 
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import org.minima.objects.base.MiniData;
 import org.minima.system.Main;
+import org.minima.system.input.InputHandler;
+import org.minima.system.network.rpc.RPCClient;
+import org.minima.utils.Crypto;
+import org.minima.utils.json.JSONObject;
 import org.minima.utils.messages.Message;
 import org.minima.utils.messages.MessageProcessor;
 
@@ -41,7 +49,47 @@ public class Maxima extends MessageProcessor {
 			
 		
 		}else if(zMessage.getMessageType().equals(MAXIMA_SENDMSG)) {
+			//Get the details..
+			String to 		= zMessage.getString("to");
+			String message	= zMessage.getString("message");
 			
+			//Hash the message..
+			//..
+			
+			//Sign the Hash using your Maxima Key
+			byte[] msgdata   = message.getBytes("UTF-8");
+			MiniData hash    = new MiniData( Crypto.getInstance().hashData(msgdata,160) );
+			
+			//For now  random.. 
+			MiniData sig     = MiniData.getRandomData(128);
+			
+			//Construct a JSON Object..
+			JSONObject msg = new JSONObject();
+			
+			msg.put("from", message);
+			msg.put("to", to);
+			msg.put("data", message);
+			msg.put("hash", hash.to0xString());
+			msg.put("signature", sig.to0xString());
+			
+			//Encode it..
+			String enc = URLEncoder.encode(new String(msg.toString()),"UTF-8").trim();
+			
+			//Store the message
+			InputHandler.getResponseJSON(zMessage).put("message", msg.toString());
+			
+			//Send it..
+			String resp = "";
+			try {
+				resp = RPCClient.sendPOST(to, enc);
+			}catch(IOException ioexc){
+				InputHandler.endResponse(zMessage, false, "Failed to connect to "+to);
+				return;
+			}
+			
+			//Reply..
+			InputHandler.getResponseJSON(zMessage).put("reply", resp);
+			InputHandler.endResponse(zMessage, true, "Message sent");
 		}
 		
 	}
