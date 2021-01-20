8bc78d28bf878fb4af40bf8265ce962b05e509db
spartacusrex <patrickcerri@gmail.com>
2021-01-20 12:31:28 +0000

    rm .minima files also in reset_master

--- scripts/reset_master.sh
+++ scripts/reset_master.sh
@@ -6,19 +6,19 @@
 #cd ..
 
 #PRIVATE COMPUTERS
-./logon.sh PRIVATE1 minima-private-1-vm --zone=europe-west4-a "cd ~ && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
-./logon.sh PRIVATE2 minima-private-2-vm --zone=europe-west4-a "cd ~ && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
+./logon.sh PRIVATE1 minima-private-1-vm --zone=europe-west4-a "cd ~ && rm -rf ~/.minima && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
+./logon.sh PRIVATE2 minima-private-2-vm --zone=europe-west4-a "cd ~ && rm -rf ~/.minima && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
 
 #PUBLIC COMPUTERS
-./logon.sh PUBLIC1 minima-public-1-vm --zone=europe-west4-a "cd ~ && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
-./logon.sh PUBLIC2 minima-public-2-vm --zone=europe-west4-a "cd ~ && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
-./logon.sh PUBLIC3 minima-public-3-vm --zone=europe-west4-a "cd ~ && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
-./logon.sh PUBLIC4 minima-public-4-vm --zone=europe-west4-a "cd ~ && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
-./logon.sh PUBLIC5 minima-public-5-vm --zone=europe-west4-a "cd ~ && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
-./logon.sh PUBLIC6 minima-public-6-vm --zone=europe-west4-a "cd ~ && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
+./logon.sh PUBLIC1 minima-public-1-vm --zone=europe-west4-a "cd ~ && rm -rf ~/.minima && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
+./logon.sh PUBLIC2 minima-public-2-vm --zone=europe-west4-a "cd ~ && rm -rf ~/.minima && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
+./logon.sh PUBLIC3 minima-public-3-vm --zone=europe-west4-a "cd ~ && rm -rf ~/.minima && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
+./logon.sh PUBLIC4 minima-public-4-vm --zone=europe-west4-a "cd ~ && rm -rf ~/.minima && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
+./logon.sh PUBLIC5 minima-public-5-vm --zone=europe-west4-a "cd ~ && rm -rf ~/.minima && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
+./logon.sh PUBLIC6 minima-public-6-vm --zone=europe-west4-a "cd ~ && rm -rf ~/.minima && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
 
 #MINERS
-./logon.sh MINER1 minima-miner-1-vm --zone=europe-west2-a "cd ~ && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
-./logon.sh MINER2 minima-miner-2-vm --zone=europe-west2-a "cd ~ && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
-./logon.sh MINER3 minima-miner-3-vm --zone=europe-west2-a "cd ~ && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
-./logon.sh MINER4 minima-miner-4-vm --zone=europe-west2-a "cd ~ && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
+./logon.sh MINER1 minima-miner-1-vm --zone=europe-west2-a "cd ~ && rm -rf ~/.minima && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
+./logon.sh MINER2 minima-miner-2-vm --zone=europe-west2-a "cd ~ && rm -rf ~/.minima && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
+./logon.sh MINER3 minima-miner-3-vm --zone=europe-west2-a "cd ~ && rm -rf ~/.minima && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
+./logon.sh MINER4 minima-miner-4-vm --zone=europe-west2-a "cd ~ && rm -rf ~/.minima && rm -rf ~/Minima && git clone https://github.com/minima-global/Minima.git" 
