761c4808fcc8ee17de1ebee99d871999b70b3424
Jerome Rousselot <jerome@jita.digital>
2021-03-24 11:08:08 +0000

    P2P: added libp2p-minimal to build process, example code and run task.

---
+++ src/org/minima/system/network/base/P2PStart.java
@@ -0,0 +1,70 @@
+package org.minima.system.network.base;
+
+import io.libp2p.core.Host;
+import io.libp2p.core.PeerId;
+import io.libp2p.core.crypto.PrivKey;
+import io.libp2p.core.dsl.Builder.Defaults;
+import io.libp2p.core.dsl.BuilderJKt;
+import io.libp2p.core.multiformats.Multiaddr;
+import io.libp2p.core.multistream.ProtocolBinding;
+import io.libp2p.core.mux.StreamMuxerProtocol;
+import io.libp2p.etc.types.ByteArrayExtKt;
+import io.libp2p.protocol.Identify;
+import io.libp2p.protocol.Ping;
+import io.libp2p.security.noise.NoiseXXSecureChannel;
+import io.libp2p.transport.tcp.TcpTransport;
+
+import io.libp2p.core.Host;
+import io.libp2p.core.dsl.HostBuilder;
+import io.libp2p.core.multiformats.Multiaddr;
+import io.libp2p.protocol.Ping;
+import io.libp2p.protocol.PingController;
+
+import java.util.concurrent.ExecutionException;
+
+// Import log4j classes.
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class P2PStart {
+
+
+    private static final Logger logger = LogManager.getLogger(P2PStart.class);
+
+    public static void main(String[] args) 
+             throws ExecutionException, InterruptedException {
+        System.out.println("Hello world!");
+       
+
+        // Create a libp2p node and configure it
+        // to accept TCP connections on a random port (0)
+        Host node = new HostBuilder()
+          .protocol(new Ping())
+          .listen("/ip4/127.0.0.1/tcp/0")
+          .build();
+
+        // start listening
+        node.start().get();
+
+    System.out.print("Node started and listening on ");
+    System.out.println(node.listenAddresses());
+
+    if (args.length > 0) {
+      Multiaddr address = Multiaddr.fromString(args[0]);
+      PingController pinger = new Ping().dial(
+          node,
+          address
+      ).getController().get();
+
+      System.out.println("Sending 5 ping messages to " + address.toString());
+      for (int i = 1; i <= 5; ++i) {
+        long latency = pinger.ping().get();
+        System.out.println("Ping " + i + ", latency " + latency + "ms");
+      }
+
+      node.stop().get();
+    }
+  }
+
+}
+
