c033f74e176e2fe066deccc528253c245c85da58
spartacusrex <patrickcerri@gmail.com>
2021-05-18 16:27:26 +0100

    add all ascii to safe emoji filter

--- src/org/minima/utils/MiniFormat.java
+++ src/org/minima/utils/MiniFormat.java
@@ -152,7 +152,7 @@ public class MiniFormat {
 //	}
 	
 	public static String filterSafeTextEmoji(String zText) {
-		String characterFilter = "[^\\|\\p{L}\\p{M}\\p{N}\\p{P}\\p{Z}\\p{Cf}\\p{Cs}\\s]";
+		String characterFilter = "[^\\x00-\\x7F\\p{L}\\p{M}\\p{N}\\p{P}\\p{Z}\\p{Cf}\\p{Cs}\\s]";
 		return zText.replaceAll(characterFilter,"");
 	}
 	
@@ -184,12 +184,11 @@ public class MiniFormat {
 	
 	public static void main(String[] zArgs) {
 		
-//		String test = "{\"version\":0.4,\"milliuptime\":2450,\"stringuptime\":\"0 Years 0 Months 0 Weeks 0 Days 0 Hours 0 Minutes 2 Seconds 450 Milliseconds\",\"conf\":\"\\/home\\/spartacusrex\\/minima\\/minima\",\"host\":\"0.0.0.0\",\"port\":9001,\"pulse\":true,\"root\":{\"block\":0,\"isblock\":true,\"txpowid\":\"0x8C73A2FF132C3242E83FA2F6AD389884A3170C0D5AE283BDEC89DD08151DD8E8\",\"parent\":\"0x0000000000000000000000000000000000000000000000000000000000000000\",\"blkdiff\":0,\"txndiff\":0,\"txn\":{\"inputs\":[],\"outputs\":[]},\"witness\":params[] pubk[] scripts[],\"txnlist\":[],\"nonce\":256,\"mmr\":\"0x7094AA8139BFBCC37CA832CADD5BB49ECCB2C54C6C1C6FC8DBAB354BA052BF66\",\"timemilli\":1573815008746},\"tip\":{\"block\":7,\"isblock\":true,\"txpowid\":\"0x188CF963491A7ED735CC60217C29598F86631503F4A7BB228AE9CAA3C85EF2EA\",\"parent\":\"0x8F50F40A85EABC440C10AB0781C39C41B3F44DC1A793C176AC4ECEB80F680AFE\",\"blkdiff\":0,\"txndiff\":0,\"txn\":{\"inputs\":[],\"outputs\":[]},\"witness\":params[] pubk[] scripts[],\"txnlist\":[],\"nonce\":-2804983266292932580,\"mmr\":\"0x7094AA8139BFBCC37CA832CADD5BB49ECCB2C54C6C1C6FC8DBAB354BA052BF66\",\"timemilli\":1573815011101},\"chainspeed\":2.9723991507431,\"lastblock\":7,\"totalpow\":8}";
+		String test = "Hello | !£$%^&*() you!";
 		
-		String test = "[{\"version\": \"0.8, , 7.67\",\"milliuptime\": 2450, googoo : [23,34,45] }]";
+//		String characterFilter = "[^[\\x00-\\x7F]\\p{L}\\p{M}\\p{N}\\p{P}\\p{Z}\\p{Cf}\\p{Cs}\\s]";
+//		return zText.replaceAll(characterFilter,"");
 		
-		String pretty = JSONPretty(test);
-		
-		System.out.println(pretty);
+		System.out.println(filterSafeTextEmoji(test));
 	}
 }
