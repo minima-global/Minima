b8fa2f6c69362cbdae437bc251488866d6aef7c9
spartacusrex <patrickcerri@gmail.com>
2021-08-03 17:14:04 +0100

    clean up

--- src/org/minima/system/brains/ConsensusNet.java
+++ src/org/minima/system/brains/ConsensusNet.java
@@ -799,24 +799,6 @@ public class ConsensusNet extends ConsensusProcessor {
 			
 			//Do we have it.. now check DB - hmmm..
 			if(getMainDB().getTxPOW(txpow.getTxPowID()) != null) {
-//				//we may have it..
-//				if(txpow.isBlock()) {
-//					MiniData parentID = txpow.getParentID();
-//					if(getMainDB().getTxPOW(parentID) == null) {
-//						//We don't have it, get it..
-//						MinimaLogger.log("(2) Request Parent TxPoW @ "+txpow.getBlockNumber()+" parent:"+parentID); 
-//						sendTxPowRequest(zMessage, parentID);
-//					}
-//				
-//					//And now check the Txn list..
-//					ArrayList<MiniData> txns = txpow.getBlockTransactions();
-//					for(MiniData txn : txns) {
-//						if(getMainDB().getTxPOW(txn) == null ) {
-//							MinimaLogger.log("(2) Request missing TxPoW in block "+txpow.getBlockNumber()+" "+txn);
-//							sendTxPowRequest(zMessage, txn);
-//						}
-//					}	
-//				}
 //				MinimaLogger.log("NET Transaction we already have.. "+txpow.getBlockNumber()+" "+txpow.getTxPowID());
 				return;
 			}
