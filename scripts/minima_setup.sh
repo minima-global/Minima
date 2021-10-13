a98f46a28204ca75a290acde35dcef33ae1a264a
Sw1fty <craigswift13@gmail.com>
2021-10-13 11:29:49 +0100

    Updated setup script for p2p testing

--- scripts/minima_setup.sh
+++ scripts/minima_setup.sh
@@ -1,26 +1,20 @@
 #!/bin/sh
 set -e
 
-P2P_ALPHA=''
 UNSECURE_FLAG=''
 CLEAN_FLAG=''
 PORT='9121'
 HOME="/home/minima"
 LOCAL="/usr/local"
-CONNECTION_HOST=''
-CONNECTION_PORT=''
 
 print_usage() {
   printf "Usage: Setups a new minima service for the specified port, default 9121 \n \t -c REQUIRED connection host and port HOST:PORT \n \t -u flag Use unsecure p2p version with rpc ports active \n \t -x flag enable clean flag \n \t -p minima port to use eg. -p 9121 \n \t -h minima home directory eg -h /home/minima \n \t -a use the p2p alphas \n"
 }
 
-while getopts ':auxc::p:h:' flag; do
+while getopts ':uxp:h:' flag; do
   case "${flag}" in
-    a) P2P_ALPHA='true';;
     u) UNSECURE_FLAG='true';;
     x) CLEAN_FLAG='true';;
-    c) CONNECTION_HOST=$(echo $OPTARG | cut -f1 -d:);
-       CONNECTION_PORT=$(echo $OPTARG | cut -f2 -d:);;
     p) PORT="${OPTARG}";;
     h) HOME="${OPTARG}";;
     *) print_usage
@@ -50,21 +44,16 @@ EOF
 systemctl restart systemd-journald
 fi
 
-DOWNLOAD_URL="https://github.com/minima-global/Minima/raw/master/jar/minima.jar"
-MINIMA_JAR_NAME="minima.jar"
-if [ $P2P_ALPHA ];
-  then
-    echo "[!] Using P2P Alpha version of minima"
-    DOWNLOAD_URL="https://github.com/minima-global/Minima/raw/feature_p2p/jar/minima-p2p-alpha.jar"
-    MINIMA_JAR_NAME="minima-p2p-alpha.jar"
-fi
+echo "[!] Using P2P Alpha version of minima"
+DOWNLOAD_URL="https://github.com/minima-global/Minima/raw/feature_p2p/jar/minima-p2p-alpha.jar"
+MINIMA_JAR_NAME="minima-p2p-alpha.jar"
 
 wget -q -O $LOCAL"/minima_service.sh" "https://github.com/minima-global/Minima/raw/feature_p2p/scripts/minima_service.sh"
 chown minima:minima $LOCAL"/minima_service.sh"
 chmod +x $LOCAL"/minima_service.sh"
 
 CRONSTRING="#!/bin/sh
-$LOCAL/minima_service.sh -s $@"
+$LOCAL/minima_service.sh -s -a -c 34.89.151.186:9121 $@"
 
 echo "$CRONSTRING" > /etc/cron.daily/minima_$PORT
 chmod a+x /etc/cron.daily/minima
