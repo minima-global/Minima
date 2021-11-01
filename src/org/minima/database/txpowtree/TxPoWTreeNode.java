0aae8451348a997fd6c84cffbeff06d2099441bc
spartacusrex <patrickcerri@gmail.com>
2021-11-01 13:31:50 +0000

    Message in logs when relevant coin created or spent

--- src/org/minima/database/txpowtree/TxPoWTreeNode.java
+++ src/org/minima/database/txpowtree/TxPoWTreeNode.java
@@ -23,6 +23,7 @@ import org.minima.objects.base.MiniData;
 import org.minima.objects.base.MiniNumber;
 import org.minima.system.brains.TxPoWSearcher;
 import org.minima.utils.Crypto;
+import org.minima.utils.MinimaLogger;
 import org.minima.utils.Streamable;
 
 public class TxPoWTreeNode implements Streamable {
@@ -148,6 +149,9 @@ public class TxPoWTreeNode implements Streamable {
 			//Is this Relevant to us..
 			if(checkRelevant(spentcoin, allrel)) {
 				mRelevantMMRCoins.add(entrynumber);
+				
+				//Message..
+				MinimaLogger.log("NEW Spent Coin : "+spentcoin.toJSON());
 			}
 		}
 		
@@ -176,11 +180,12 @@ public class TxPoWTreeNode implements Streamable {
 			//Add to the total List of coins fro this block
 			mCoins.add(newcoin);
 			
-//			MinimaLogger.log("TxPoWTreeNode New Coin rel:"+zFindRelevant+" "+newcoin.toJSON().toString());
-			
 			//Is this Relevant to us..
 			if(checkRelevant(output, allrel)) {
 				mRelevantMMRCoins.add(entrynumber);
+				
+				//Message..
+				MinimaLogger.log("NEW Unspent Coin : "+newcoin.toJSON());
 			}
 		}
 		
