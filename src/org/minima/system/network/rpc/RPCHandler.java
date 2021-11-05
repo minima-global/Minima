191e78f35d40d169db73a1d66906c9022411749e
spartacusrex <patrickcerri@gmail.com>
2021-11-05 13:36:10 +0000

    fix imports

--- src/org/minima/system/network/rpc/RPCHandler.java
+++ src/org/minima/system/network/rpc/RPCHandler.java
@@ -1,7 +1,6 @@
 package org.minima.system.network.rpc;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
