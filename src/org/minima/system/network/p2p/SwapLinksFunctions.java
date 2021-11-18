868a261157071ba915f53a28c67f7043236eff2f
Sw1fty <craigswift13@gmail.com>
2021-11-18 10:38:23 +0000

    Fixed the removal of disconnected links code

--- src/org/minima/system/network/p2p/SwapLinksFunctions.java
+++ src/org/minima/system/network/p2p/SwapLinksFunctions.java
@@ -88,24 +88,12 @@ public class SwapLinksFunctions {
     public static void onDisconnected(P2PState state, Message zMessage) {
         //Get the details
         String uid = zMessage.getString("uid");
-        boolean incoming = zMessage.getBoolean("incoming");
-        boolean reconnect = zMessage.getBoolean("reconnect");
 
         // Remove uid from current connections
-        InetSocketAddress removedAddress = null;
-        if (incoming) {
-
-            removedAddress = state.getInLinks().remove(uid);
-            if (removedAddress == null) {
-                removedAddress = state.getNotAcceptingConnP2PLinks().remove(uid);
-            }
-            if (removedAddress == null) {
-                removedAddress = state.getNoneP2PLinks().remove(uid);
-            }
-        } else {
-            removedAddress = state.getOutLinks().remove(uid);
-        }
-
+        state.getInLinks().remove(uid);
+        state.getNotAcceptingConnP2PLinks().remove(uid);
+        state.getOutLinks().remove(uid);
+        state.getNoneP2PLinks().remove(uid);
     }
 
     public static void updateKnownPeersFromGreeting(P2PState state, P2PGreeting greeting) {
