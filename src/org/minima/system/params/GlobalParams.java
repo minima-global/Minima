c9da2b0add30be9d0d2cc6a756c942e9ab40ea97
Sw1fty <craigswift13@gmail.com>
2021-11-19 10:13:22 +0000

    changed version number

--- src/org/minima/system/params/GlobalParams.java
+++ src/org/minima/system/params/GlobalParams.java
@@ -7,7 +7,7 @@ public class GlobalParams {
 	/**
 	 * Which Version
 	 */
-	public static String MINIMA_VERSION = "0.100.12";
+	public static String MINIMA_VERSION = "0.100.13";
 	
 	/**
 	 * Speed in blocks per second.. 
