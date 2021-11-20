06706df8a11c0a37ea94be445d95c6e92372a550
mcksn <mackson.ogu@gmail.com>
2021-11-20 15:20:33 +0000

    refactoring param configurer to allow for env variables source

--- src/org/minima/Minima.java
+++ src/org/minima/Minima.java
@@ -1,27 +1,22 @@
 package org.minima;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-
 import org.minima.database.MinimaDB;
 import org.minima.objects.base.MiniString;
 import org.minima.system.Main;
 import org.minima.system.commands.Command;
+import org.minima.system.params.ParamConfigurer;
 import org.minima.system.params.GeneralParams;
 import org.minima.system.params.GlobalParams;
-import org.minima.system.params.TestParams;
 import org.minima.utils.MiniFormat;
 import org.minima.utils.MinimaLogger;
 import org.minima.utils.json.JSONArray;
 import org.minima.utils.json.JSONObject;
 
-import static java.nio.file.Files.lines;
-import static java.util.Arrays.stream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
 
 public class Minima {
 
@@ -73,13 +68,20 @@ public class Minima {
 		File dataFolder = new File(System.getProperty("user.home"),".minima");
 		GeneralParams.DATA_FOLDER = dataFolder.getAbsolutePath();
 
-		Configurer configurer = new Configurer()
-				.configureFrom(argsFromConfFile(zArgs))
-				.configureFrom(zArgs); // executable args override conf file
+		ParamConfigurer configurer = null;
+		try {
+			configurer = new ParamConfigurer()
+					.usingConfFile(zArgs)
+					.usingEnvVariables(System.getenv())
+					.usingProgramArgs(zArgs)
+					.configure();
+		} catch (ParamConfigurer.UnknownArgumentException ex) {
+			System.out.println(ex);
+			System.exit(1);
+		}
 
-		//Daemon mode
-		boolean daemon = configurer.daemon;
-		boolean rpcenable = configurer.rpcenable;
+		boolean daemon = configurer.isDaemon();
+		boolean rpcenable = configurer.isRpcenable();
 
 		MinimaLogger.log("**********************************************");
 		MinimaLogger.log("*  __  __  ____  _  _  ____  __  __    __    *");
@@ -173,135 +175,5 @@ public class Minima {
 	    }
 	    
 	    MinimaLogger.log("Bye bye..");
-	}
-
-	private static String[] argsFromConfFile(String[] programArgs) {
-		List<String> zArgsList = Arrays.asList(programArgs);
-		int confArgKeyIndex = zArgsList.indexOf("-conf");
-
-		if (confArgKeyIndex > -1) {
-			final String confFileArgValue = zArgsList.get(confArgKeyIndex + 1);
-			File confFile = new File(confFileArgValue);
-
-			try {
-				return lines(confFile.toPath())
-						.filter(line -> !line.startsWith("#")) // skip comments
-						.flatMap(line -> {
-							final String[] keyValue = line.split("=");
-							keyValue[0] = "-" + keyValue[0]; // convert key to arg format
-							return stream(keyValue);
-						})
-						.toArray(String[]::new);
-			} catch (IOException exception) {
-				System.out.println("Enable to read conf file.");
-				System.exit(1);
-				return new String[] {};
-			}
-		} else
-			return new String[] {};
-	}
-
-	private static class Configurer {
-		private boolean daemon = false;
-		private boolean rpcenable = false;
-
-		private Configurer configureFrom(String[] zArgs) {
-			int arglen = zArgs.length;
-			if (arglen > 0) {
-				int counter = 0;
-				while (counter < arglen) {
-					String arg = zArgs[counter];
-					counter++;
-
-					if (arg.equals("-conf")) {
-						counter++; // skip
-
-					} else if (arg.equals("-port")) {
-						GeneralParams.MINIMA_PORT = Integer.parseInt(zArgs[counter++]);
-
-					} else if (arg.equals("-host")) {
-						GeneralParams.MINIMA_HOST = zArgs[counter++];
-						GeneralParams.IS_HOST_SET = true;
-
-					} else if (arg.equals("-rpc")) {
-						GeneralParams.RPC_PORT = Integer.parseInt(zArgs[counter++]);
-
-					}else if(arg.equals("-rpcenable")) {
-						rpcenable = true;
-
-					} else if (arg.equals("-data")) {
-						GeneralParams.DATA_FOLDER = zArgs[counter++];
-
-					} else if (arg.equals("-daemon")) {
-						daemon = true;
-
-					} else if (arg.equals("-private")) {
-						GeneralParams.PRIVATE_NETWORK = true;
-
-					} else if (arg.equals("-noconnect")) {
-						GeneralParams.NOCONNECT = true;
-
-					} else if (arg.equals("-nop2p")) {
-						GeneralParams.P2P_ENABLED = false;
-
-					} else if (arg.equals("-p2pnode")) {
-						GeneralParams.P2P_ROOTNODE = zArgs[counter++];
-
-					} else if (arg.equals("-automine")) {
-						GeneralParams.AUTOMINE = true;
-
-					} else if (arg.equals("-connect")) {
-						GeneralParams.P2P_ENABLED = false;
-						GeneralParams.CONNECT_LIST = zArgs[counter++];
-
-					} else if (arg.equals("-noautomine")) {
-						GeneralParams.AUTOMINE = false;
-
-					} else if (arg.equals("-clean")) {
-						GeneralParams.CLEAN = true;
-
-					} else if (arg.equals("-genesis")) {
-						GeneralParams.CLEAN = true;
-						GeneralParams.PRIVATE_NETWORK = true;
-						GeneralParams.GENESIS = true;
-						GeneralParams.AUTOMINE = true;
-
-					} else if (arg.equals("-test")) {
-						GeneralParams.TEST_PARAMS = true;
-						GeneralParams.PRIVATE_NETWORK = true;
-						TestParams.setTestParams();
-
-					} else if (arg.equals("-help")) {
-
-						System.out.println("Minima Help");
-						System.out.println(" -host       : Specify the host IP");
-						System.out.println(" -port       : Specify the Minima port");
-						System.out.println(" -rpc        : Specify the RPC port");
-						System.out.println(" -conf       : Specify a configuration file (absolute)");
-						System.out.println(" -data       : Specify the data folder (absolute) (defaults to .minima/ under user home");
-						System.out.println(" -daemon     : Run in daemon mode with no stdin input ( services )");
-						System.out.println(" -mobile     : Sets this device to a mobile device - used for metrics only");
-						System.out.println(" -nop2p      : Disable the automatic P2P system");
-						System.out.println(" -isclient   : Tells the P2P System that this node can't accept incoming connections");
-						System.out.println(" -noconnect  : Stops the P2P system from connecting to other nodes until it's been connected too");
-						System.out.println(" -p2pnode    : Specify the initial P2P host:port list to connect to");
-						System.out.println(" -automine   : Simulate user traffic to construct the blockchain");
-						System.out.println(" -noautomine : Do not simulate user traffic to construct the blockchain");
-						System.out.println(" -clean      : Wipe data folder at startup");
-						System.out.println(" -genesis    : Create a genesis block, -clean and -automine");
-						System.out.println(" -connect    : Disable the p2p and manually connect to this list of host:port");
-						System.out.println(" -test       : Use test params");
-						System.out.println(" -help       : Print this help");
-						System.exit(1);
-
-					} else {
-						System.out.println("Unknown parameter : " + arg);
-						System.exit(1);
-					}
-				}
-			}
-			return this;
-		}
-
 	}
 }
