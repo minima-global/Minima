e4c5c8eb6163064a7606a9c4e1efc0c279a8afb9
Sw1fty <craigswift13@gmail.com>
2021-11-22 13:39:50 +0000

    Set setMaxNumP2PConnections correctly - version to 100.16

--- src/org/minima/system/params/GlobalParams.java
+++ src/org/minima/system/params/GlobalParams.java
@@ -7,7 +7,7 @@ public class GlobalParams {
 	/**
 	 * Which Version
 	 */
-	public static String MINIMA_VERSION = "0.100.15";
+	public static String MINIMA_VERSION = "0.100.16";
 	
 	/**
 	 * Speed in blocks per second.. 
