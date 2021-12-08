a16c8d663861064d7bd1be02c5dd47fa8ce898e6
Sw1fty <craigswift13@gmail.com>
2021-12-08 12:02:49 +0000

    Reordered cleanup script

--- scripts/minima_cleanup_v98.sh
+++ scripts/minima_cleanup_v98.sh
@@ -11,10 +11,10 @@ rm /etc/systemd/system/minima.service
 systemctl daemon-reload
 systemctl reset-failed
 
+echo "Removing data directory /root/.minima"
+rm -rf /root/.minima
+
 echo "Removing minima jars and scripts"
 rm minima.jar*
 rm minima_update.sh*
 rm minima_service.sh*
-
-echo "Removing data directory /root/.minima"
-rm -rf /root/.minima
