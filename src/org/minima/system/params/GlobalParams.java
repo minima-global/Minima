08255c5919deb3ad2e26b86c004db90bde8b0b6e
spartacusrex <patrickcerri@gmail.com>
2022-01-05 13:24:31 +0000

    version upgrade

--- src/org/minima/system/params/GlobalParams.java
+++ src/org/minima/system/params/GlobalParams.java
@@ -7,7 +7,7 @@ public class GlobalParams {
 	/**
 	 * Which Version
 	 */
-	public static String MINIMA_VERSION = "0.100.25";
+	public static String MINIMA_VERSION = "0.100.26";
 	
 	/**
 	 * Speed in blocks per second.. 
