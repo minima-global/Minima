2e5fbd620b025d16ba9e0a681180eb919a507d25
spartacusrex <patrickcerri@gmail.com>
2022-02-01 12:44:01 +0000

    fix functions, change PROOF to return boolean

--- src/org/minima/kissvm/functions/sha/PROOF.java
+++ src/org/minima/kissvm/functions/sha/PROOF.java
@@ -7,9 +7,12 @@ import org.minima.database.mmr.MMRProof;
 import org.minima.kissvm.Contract;
 import org.minima.kissvm.exceptions.ExecutionException;
 import org.minima.kissvm.functions.MinimaFunction;
+import org.minima.kissvm.values.BooleanValue;
 import org.minima.kissvm.values.HexValue;
+import org.minima.kissvm.values.StringValue;
 import org.minima.kissvm.values.Value;
 import org.minima.objects.base.MiniData;
+import org.minima.utils.Crypto;
 
 public class PROOF extends MinimaFunction {
 
@@ -21,14 +24,33 @@ public class PROOF extends MinimaFunction {
 	public Value runFunction(Contract zContract) throws ExecutionException {
 		checkExactParamNumber(requiredParams());
 		
-		//Get the initial data
-		MiniData data = zContract.getHexParam(0, this).getMiniData();
-
+		//Get the initial data - can be a string or HEX
+		Value vv = getParameter(0).getValue(zContract);
+		checkIsOfType(vv, Value.VALUE_HEX | Value.VALUE_SCRIPT);
+		
+		MiniData data = null;
+		if(vv.getValueType() == Value.VALUE_HEX) {
+			//HEX
+			HexValue hex = (HexValue)vv;
+			data 		 = hex.getMiniData();
+			
+		}else {
+			//Script..
+			StringValue scr = (StringValue)vv;
+			data 			= new MiniData(scr.getBytes());
+		}
+		
+		//Hash the data
+		MiniData hash 	= Crypto.getInstance().hashObject(data);
+		
 		//Create an MMRData object - 0 value..
-		MMRData mmrdata = new MMRData(data);
+		MMRData mmrdata = new MMRData(hash);
 		
 		//Get the proof chain 
 		HexValue chain = zContract.getHexParam(1, this);
+		
+		//The root of the tree
+		MMRData mmrroot = new MMRData(zContract.getHexParam(2, this).getMiniData());
 		
 		//Create into the MMRProof..
 		MMRProof proof = null;
@@ -41,14 +63,17 @@ public class PROOF extends MinimaFunction {
 		
 		//And calculate the final chain value..
 		MMRData root = proof.calculateProof(mmrdata); 
-				
+		
+		//Are they the same..
+		boolean same = root.isEqual(mmrroot);
+		
 		//Return..
-		return new HexValue(root.getData());
+		return new BooleanValue(same);
 	}
 	
 	@Override
 	public int requiredParams() {
-		return 2;
+		return 3;
 	}
 	
 	@Override
