f3b06a14c427aea45201b5f02ff7222a58ce9212
spartacusrex <patrickcerri@gmail.com>
2022-02-01 12:51:25 +0000

    Add different hash types

--- src/org/minima/system/commands/base/hash.java
+++ src/org/minima/system/commands/base/hash.java
@@ -3,13 +3,14 @@ package org.minima.system.commands.base;
 import org.minima.objects.base.MiniData;
 import org.minima.objects.base.MiniString;
 import org.minima.system.commands.Command;
+import org.minima.system.commands.CommandException;
 import org.minima.utils.Crypto;
 import org.minima.utils.json.JSONObject;
 
 public class hash extends Command {
 
 	public hash() {
-		super("hash","[data:] - Hash the data using KECCAK 256");
+		super("hash","[data:] (type:keccak|sha2|sha3)- Hash the data - default KECCAK");
 	}
 	
 	@Override
@@ -25,10 +26,29 @@ public class hash extends Command {
 			data = new MiniData(new MiniString(datastr).getData());
 		}
 		
-		byte[] hash = Crypto.getInstance().hashData(data.getBytes());
+		String hashtype = getParam("type", "keccak");
+	
+		byte[] hash = null;
+		if(hashtype.equals("keccak")) {
+			hash = Crypto.getInstance().hashData(data.getBytes());
 		
+		}else if(hashtype.equals("sha2")) {
+			hash = Crypto.getInstance().hashSHA2(data.getBytes());
 		
-		ret.put("response", new MiniData(hash).to0xString());
+		}else if(hashtype.equals("sha3")) {
+			hash = Crypto.getInstance().hashSHA3(data.getBytes());
+		
+		}else {
+			throw new CommandException("Invalid hash type : "+hashtype);
+		}
+		
+		JSONObject resp = new JSONObject();
+		resp.put("input", datastr);
+		resp.put("data", data.to0xString());
+		resp.put("type", hashtype);
+		resp.put("hash", new MiniData(hash).to0xString());
+		
+		ret.put("response", resp);
 		
 		return ret;
 	}
