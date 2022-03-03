97e09574cf75dfa065eeb870f87c5a36ff0580ac
spartacusrex <patrickcerri@gmail.com>
2022-03-03 14:20:04 +0000

    set acceptable time for TxPoW +1 block

--- src/org/minima/system/brains/TxPoWGenerator.java
+++ src/org/minima/system/brains/TxPoWGenerator.java
@@ -74,7 +74,7 @@ public class TxPoWGenerator {
 			//How much time to add to the median block
 			MiniNumber blocksecs 	= MiniNumber.ONE.div(GlobalParams.MINIMA_BLOCK_SPEED);
 			MiniNumber half 		= new MiniNumber(TxPoWChecker.MEDIAN_BLOCK).div(MiniNumber.TWO); 
-			MiniNumber addtime 		= blocksecs.mult(half).mult(MiniNumber.THOUSAND);
+			MiniNumber addtime 		= blocksecs.mult(half.add(MiniNumber.ONE)).mult(MiniNumber.THOUSAND);
 			
 			//Median time + 1 hr..
 			txpow.setTimeMilli(medianblock.getTimeMilli().add(addtime));
