20a0429106d2e77d2d06dfacc66dfb8eec35f245
spartacusrex <patrickcerri@gmail.com>
2022-03-08 15:57:53 +0000

    GET can be full of any value type..number, hex, string..

--- src/org/minima/kissvm/functions/general/GET.java
+++ src/org/minima/kissvm/functions/general/GET.java
@@ -17,7 +17,7 @@ public class GET extends MinimaFunction{
 	@Override
 	public Value runFunction(Contract zContract) throws ExecutionException {
 		checkMinParamNumber(requiredParams());
-		checkAllParamsType(Value.VALUE_NUMBER, zContract);
+//		checkAllParamsType(Value.VALUE_NUMBER, zContract);
 		
 		//The final Param String to search for
 		String ps = "";
