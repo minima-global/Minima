2bf2f8da5f6fa7e3496ab341684e2bebbfb2f369
spartacusrex <patrickcerri@gmail.com>
2022-06-01 11:41:18 +0100

    0.102.10

--- src/org/minima/system/params/GlobalParams.java
+++ src/org/minima/system/params/GlobalParams.java
@@ -7,7 +7,7 @@ public class GlobalParams {
 	/**
 	 * Which Version
 	 */
-	public static String MINIMA_VERSION = "0.102.9";
+	public static String MINIMA_VERSION = "0.102.10";
 	
 	/**
 	 * Speed in blocks per second.. 
