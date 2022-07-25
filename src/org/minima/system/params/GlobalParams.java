03b1491a515bd0e6a1279ecf49b0d8775793a4da
spartacusrex <patrickcerri@gmail.com>
2022-07-25 15:26:10 +0100

    longer chain kept by users

--- src/org/minima/system/params/GlobalParams.java
+++ src/org/minima/system/params/GlobalParams.java
@@ -35,7 +35,7 @@ public class GlobalParams {
 	/**
 	 * Depth before we cascade..
 	 */
-	public static MiniNumber MINIMA_CASCADE_START_DEPTH = new MiniNumber(1024);
+	public static MiniNumber MINIMA_CASCADE_START_DEPTH = new MiniNumber(2048);
 	
 	/**
 	 * Number of blocks at each cascade level 
