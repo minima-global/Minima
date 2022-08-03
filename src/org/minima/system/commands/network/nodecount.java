82b98221f5657b2285d734014846532b2df97f27
Sw1fty <craigswift13@gmail.com>
2022-08-03 11:19:04 +0100

    Added node count function

---
+++ src/org/minima/system/commands/network/nodecount.java
@@ -0,0 +1,79 @@
+package org.minima.system.commands.network;
+
+import org.minima.objects.Greeting;
+import org.minima.system.Main;
+import org.minima.system.commands.Command;
+import org.minima.system.commands.CommandException;
+import org.minima.system.network.minima.NIOClientInfo;
+import org.minima.system.network.minima.NIOManager;
+import org.minima.system.network.p2p.messages.InetSocketAddressIO;
+import org.minima.system.network.p2p.params.P2PParams;
+import org.minima.utils.MinimaLogger;
+import org.minima.utils.json.JSONArray;
+import org.minima.utils.json.JSONObject;
+
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class nodecount extends Command {
+
+	public nodecount() {
+		super("nodecount","");
+	}
+	
+	@Override
+	public JSONObject runCommand() throws Exception {
+		JSONObject ret = getJSONReply();
+		Set<InetSocketAddress> allPeers = new HashSet<>();
+		Set<InetSocketAddress> pingedPeers = new HashSet<>();
+
+		for (InetSocketAddress addr: P2PParams.DEFAULT_NODE_LIST)
+		{
+			Greeting greet = NIOManager.sendPingMessage(addr.getHostString(), addr.getPort(), true);
+			if (greet != null) {
+				JSONArray peersArrayList = (JSONArray) greet.getExtraData().get("peers-list");
+				if (peersArrayList != null) {
+//                    MinimaLogger.log(peersArrayList.toString());
+					List<InetSocketAddress> newPeers = InetSocketAddressIO.addressesJSONArrayToList(peersArrayList);
+					allPeers.addAll(newPeers);
+				}
+			}
+			pingedPeers.add(addr);
+		}
+		boolean keepGoing = true;
+		Set<InetSocketAddress> peersToCheck = new HashSet<>(allPeers);
+		peersToCheck.removeAll(pingedPeers);
+
+		while (keepGoing){
+			for (InetSocketAddress addr: peersToCheck){
+				Greeting greet = NIOManager.sendPingMessage(addr.getHostString(), addr.getPort(), true);
+				if (greet != null) {
+					JSONArray peersArrayList = (JSONArray) greet.getExtraData().get("peers-list");
+					if (peersArrayList != null) {
+//                    MinimaLogger.log(peersArrayList.toString());
+						List<InetSocketAddress> newPeers = InetSocketAddressIO.addressesJSONArrayToList(peersArrayList);
+						allPeers.addAll(newPeers);
+					}
+				}
+				pingedPeers.add(addr);
+				MinimaLogger.log("Total unique peers: " + allPeers.size() + " Peers Checked: " + pingedPeers.size());
+			}
+			peersToCheck = new HashSet<>(allPeers);
+			peersToCheck.removeAll(pingedPeers);
+			if (peersToCheck.size() == 0){
+				keepGoing = false;
+			}
+		}
+
+		return ret;
+	}
+
+	@Override
+	public Command getFunction() {
+		return new nodecount();
+	}
+
+}
