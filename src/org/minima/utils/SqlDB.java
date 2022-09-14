1afc749e6cb31a692e3328f6287075bb3225aa4b
spartacusrex <patrickcerri@gmail.com>
2022-09-14 13:11:52 +0100

    only COMPACT the db on start up..

--- src/org/minima/utils/SqlDB.java
+++ src/org/minima/utils/SqlDB.java
@@ -80,7 +80,7 @@ public abstract class SqlDB {
 			Statement stmt = mSQLConnection.createStatement();
 		
 			//Shut down.. this saves and closes all the data
-			stmt.execute("SHUTDOWN COMPACT");
+			stmt.execute("SHUTDOWN");
 
 			//Close the connection
 			mSQLConnection.close();
