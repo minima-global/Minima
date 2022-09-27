583713f8db029692fab0cd21b917b799216d8454
Aminima <92369383+aminimaglobal@users.noreply.github.com>
2022-09-27 12:45:53 +0100

    Fixed raw url
    Previously getting the github page instead of the raw document

--- scripts/minima_setup.sh
+++ scripts/minima_setup.sh
@@ -45,7 +45,7 @@ if ! id -u 9001 > /dev/null 2>&1; then
     chown minima:minima $HOME
 fi
 
-wget -q -O $HOME"/minima_service.sh" "https://github.com/minima-global/Minima/blob/test-mdsenable-scripts/scripts/minima_service.sh"
+wget -q -O $HOME"/minima_service.sh" "https://raw.githubusercontent.com/minima-global/Minima/test-mdsenable-scripts/scripts/minima_service.sh"
 chown minima:minima $HOME"/minima_service.sh"
 chmod +x $HOME"/minima_service.sh"
 
