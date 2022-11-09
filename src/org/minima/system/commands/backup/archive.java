77241759c7f288a64e8d56161a10d62b9fb53619
spartacusrex <patrickcerri@gmail.com>
2022-11-09 10:48:39 +0000

    missing @Override

--- src/org/minima/system/commands/backup/archive.java
+++ src/org/minima/system/commands/backup/archive.java
@@ -45,6 +45,7 @@ public class archive extends Command {
 		return new ArrayList<>(Arrays.asList(new String[]{"action","host","phrase","keys","keyuses"}));
 	}
 	
+	@Override
 	public String getFullHelp() {
 		return "\narchive\n"
 				+ "\n"
