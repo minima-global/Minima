93c80ba8a3c8420c5553e27a75749317a514e01f
oskii <oscar.chambers@minima.global>
2022-11-23 12:33:01 +0100

    add tests for txncreate and txnsign minima commands, with regards to the new style of testing which will spin up a static minima node at the start of each test class

--- test/org/minima/tests/cli/txncreate/TxncreateTest.java
+++ test/org/minima/tests/cli/txncreate/TxncreateTest.java
@@ -23,11 +23,80 @@ public class TxncreateTest extends MinimaCliTest {
         String output = super.minimaTestNode.runCommand("txncreate");
         runBaseTests(output);        
     }
+
+    @Test
+    public void testTxncreateWithValidArgs () throws Exception 
+    {
+        String output = super.minimaTestNode.runCommand("txncreate id:\"myTransaction\"");
+
+        runBaseTestsWithValidArgs (output);
+    }
+
+    @Test
+    public void testTxncreateTwiceWithSameID () throws Exception 
+    {
+        super.minimaTestNode.runCommand("txncreate id:\"myTransaction\"");
+        
+        String output = super.minimaTestNode.runCommand("txncreate id:\"myTransaction\"");
+
+        runBaseTestsWithInvalidArgs (output);
+    }
+    
+    @Test
+    public void testTxncreateWithNoDataInIDArgument () throws Exception 
+    {        
+        String output = super.minimaTestNode.runCommand("txncreate id:\"\"");
+
+        runBaseTestsWithInvalidArgs (output);
+    }
+
+    @Test
+    public void testTxncreateWithLongIDArgument () throws Exception 
+    {        
+        String output = super.minimaTestNode.runCommand("txncreate id:\"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\"");
+
+        runBaseTestsWithValidArgs (output);
+    }  
+    
+    public void testTxncreateWithApostropheAsIDArgument () throws Exception 
+    {        
+        String output = super.minimaTestNode.runCommand("txncreate id:\"'\"");
+
+        runBaseTestsWithInvalidArgs (output);
+    }
     
     public void runBaseTests (String output) throws Exception
     {
         //The cmd response should be valid JSON
         JSONObject json = (JSONObject) new JSONParser().parse(output);
+
+        //status of the cmd request must be true
+        assertFalse("response status should be false", (boolean)json.get("status"));
+
+        //cmd response pending should be false
+        assertFalse("response pending should be false", (boolean)json.get("pending"));
+    }
+
+    public void runBaseTestsWithValidArgs (String output) throws Exception 
+    {
+        //The cmd response should be valid JSON
+        JSONObject json = (JSONObject) new JSONParser().parse(output);
+
+        //status of the cmd request must be true
+        System.out.println("status must be true: " + json.get("status"));
+        assertTrue("response status should be true", (boolean)json.get("status"));
+
+        //cmd response pending should be false
+        assertFalse("response pending should be false", (boolean)json.get("pending"));
+    }
+
+    public void runBaseTestsWithInvalidArgs (String output) throws Exception 
+    {
+        //The cmd response should be valid JSON
+        JSONObject json = (JSONObject) new JSONParser().parse(output);
+
+        System.out.println("THE OUTPUT OF THE TEST ");
+        System.out.println(output);
 
         //status of the cmd request must be true
         System.out.println("status must be false: " + json.get("status"));
