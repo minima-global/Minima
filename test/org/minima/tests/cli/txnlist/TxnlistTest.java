abd9644fee3332e7e906604b3c78d92efdac7140
oskii <oscar.chambers@minima.global>
2022-11-24 20:31:34 +0100

    Add Tnxclear and Txnlist cli command tests

--- test/org/minima/tests/cli/txnlist/TxnlistTest.java
+++ test/org/minima/tests/cli/txnlist/TxnlistTest.java
@@ -21,12 +21,91 @@ public class TxnlistTest extends MinimaCliTest {
     public void testTxnlistWithNoArgs () throws Exception
     {
         String output = super.minimaTestNode.runCommand("txnlist");
-        runBaseTests(output);        
+        runBaseTestsWithValidArgs(output);        
+    }
+
+    @Test
+    public void testTxnlistWithInvalidArgs () throws Exception
+    {
+        String output = super.minimaTestNode.runCommand("txnlist arg:invalid");
+        runBaseTestsWithInvalidArgs(output);        
+    }
+
+    @Test
+    public void testTxnlistWithOneTransaction () throws Exception 
+    {
+
+        String chosenTxId = "mytransaction";
+
+        super.minimaTestNode.runCommand("txncreate id:" + chosenTxId);
+
+        String output = super.minimaTestNode.runCommand("txnlist");
+
+        runBaseTestsWithValidArgs (output);
+
+        JSONObject jsonOutput =  (JSONObject) new JSONParser().parse(output.toString());
+
+        JSONArray jsonArray = (JSONArray) jsonOutput.get("response");
+
+        JSONObject zeroth = (JSONObject) jsonArray.get(0);
+
+        String firstTransactionId = zeroth.get("id").toString();
+
+        assertTrue("The transaction id in the list should match the transaction id specified in txncreate ",firstTransactionId.equals(chosenTxId));
+
+    }
+
+    @Test
+    public void testTxnlistWithManyTransactions () throws Exception 
+    {
+
+        int count = 0;
+        int num_new_transactions = 100;
+        String chosenTxIdBase = "mytransaction";
+
+        //add 100 transactions
+        while(++count <= num_new_transactions)
+        {
+            
+            super.minimaTestNode.runCommand("txncreate id:" + chosenTxIdBase + Integer.toString(count));
+        }        
+
+        String output = super.minimaTestNode.runCommand("txnlist");
+
+        runBaseTestsWithValidArgs (output);
+
+        JSONObject jsonOutput =  (JSONObject) new JSONParser().parse(output.toString());
+
+        JSONArray jsonArray = (JSONArray) jsonOutput.get("response");
+
+        count = 0;
+
+        while(++count <= num_new_transactions)
+        {
+
+            JSONObject nth_transaction = (JSONObject) jsonArray.get(count);
+
+            String nthTransactionId = nth_transaction.get("id").toString();
+
+            assertTrue("The transaction id in the list should match the transaction id specified in txncreate ", nthTransactionId.equals(chosenTxIdBase + Integer.toString(count)));
+        }
     }
     
-    public void runBaseTests (String output) throws Exception
+    public void runBaseTestsWithValidArgs (String output) throws Exception
     {
         super.runBaseTests(output);
+    }
+
+    public void runBaseTestsWithInvalidArgs (String output) throws Exception
+    {
+        //The cmd response should be valid JSON
+        JSONObject json = (JSONObject) new JSONParser().parse(output);
+
+        //status of the cmd request must be true
+        assertFalse("status must be false: ", (boolean)json.get("status"));
+
+        //cmd response pending should be false
+        assertFalse("pending must be false: ", (boolean)json.get("pending"));
     }
 
 }
