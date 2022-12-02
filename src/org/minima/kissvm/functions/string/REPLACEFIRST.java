3b4df0bcccdce1cc8abd9dad5a9b8547488073c1
spartacusrex <patrickcerri@gmail.com>
2022-12-02 14:30:28 +0000

    Replace only the FIRST occurrence

---
+++ src/org/minima/kissvm/functions/string/REPLACEFIRST.java
@@ -0,0 +1,47 @@
+package org.minima.kissvm.functions.string;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.minima.kissvm.Contract;
+import org.minima.kissvm.exceptions.ExecutionException;
+import org.minima.kissvm.functions.MinimaFunction;
+import org.minima.kissvm.values.StringValue;
+import org.minima.kissvm.values.Value;
+import org.minima.utils.MinimaLogger;
+
+public class REPLACEFIRST extends MinimaFunction {
+
+	public REPLACEFIRST() {
+		super("REPLACEFIRST");
+	}
+
+	@Override
+	public Value runFunction(Contract zContract) throws ExecutionException {
+		checkExactParamNumber(requiredParams());
+
+		//Get the the first string
+		StringValue strmain   	= zContract.getStringParam(0, this);
+		StringValue strsearch 	= zContract.getStringParam(1, this);
+		StringValue strrepl 	= zContract.getStringParam(2, this);
+
+		String main 	= strmain.toString();
+		String search 	= strsearch.toString();
+		String repl 	= strrepl.toString();
+
+		//Now replace..
+		String newstr = main.replaceFirst(search, repl);
+		
+		return new StringValue(newstr);	
+	}
+
+	@Override
+	public int requiredParams() {
+		return 3;
+	}
+	
+	@Override
+	public MinimaFunction getNewFunction() {
+		return new REPLACEFIRST();
+	}
+}
