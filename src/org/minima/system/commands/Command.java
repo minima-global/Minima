ace64babc4b9580a6d7cbd51fb796079f5e469f9
spartacusrex <patrickcerri@gmail.com>
2023-02-07 08:02:17 +0000

    Add rpc to WRITE commands

--- src/org/minima/system/commands/Command.java
+++ src/org/minima/system/commands/Command.java
@@ -722,7 +722,7 @@ public abstract class Command {
 	private static final String[] ALL_WRITE_COMMANDS = 
 		{"send","sendpoll","multisig","tokencreate","consolidate",
 		 "cointrack","sign","txnsign","mds","backup",
-		 "restore","vault","archive","maxsign","mysql"};
+		 "restore","vault","archive","maxsign","mysql","rpc"};
 	
 	private static final ArrayList<String> ALL_WRITE_COMMANDS_ARRAY = new ArrayList<String>(Arrays.asList(ALL_WRITE_COMMANDS));
 	
