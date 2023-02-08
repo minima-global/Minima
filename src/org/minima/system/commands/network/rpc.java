0ff2498cb901d058dab3f315ecbe05ed0856dee1
patrickcerri <patrickcerri@patricks-air>
2023-02-08 11:02:43 +0000

    fix rpc help

--- src/org/minima/system/commands/network/rpc.java
+++ src/org/minima/system/commands/network/rpc.java
@@ -11,7 +11,7 @@ import org.minima.utils.json.JSONObject;
 public class rpc extends Command {
 
 	public rpc() {
-		super("rpc","[enable:true|false] - Enable and disable RPC on port "+GeneralParams.RPC_PORT+" (default is off)");
+		super("rpc","(enable:) (ssl:) (password) - Enable and disable RPC on port "+GeneralParams.RPC_PORT+" (default is off)");
 	}
 	
 	@Override
@@ -25,9 +25,19 @@ public class rpc extends Command {
 				+ "enable:\n"
 				+ "    true or false, true to enable rpc or false to disable.\n"
 				+ "\n"
+				+ "ssl:\n"
+				+ "    true or false, true to enable Self signed SSL - you can use stunnel yourself.\n"
+				+ "\n"
+				+ "password:\n"
+				+ "    the Basic Auth password used in headers - only secure if used with SSL.\n"
+				+ "\n"
 				+ "Examples:\n"
 				+ "\n"
 				+ "rpc enable:true\n"
+				+ "\n"
+				+ "rpc enable:true ssl:true password:minima\n"
+				+ "\n"
+				+ "rpc enable:true password:minima\n"
 				+ "\n"
 				+ "rpc enable:false\n";
 	}
