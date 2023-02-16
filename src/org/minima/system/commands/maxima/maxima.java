d0b8f9063535daf3bd92c4947276e58077efeeeb
spartacusrex <patrickcerri@gmail.com>
2023-02-16 10:33:46 +0000

    new sendall action

--- src/org/minima/system/commands/maxima/maxima.java
+++ src/org/minima/system/commands/maxima/maxima.java
@@ -309,6 +309,50 @@ public class maxima extends Command {
 				ret.put("response", json);
 			}
 		
+		}else if(func.equals("sendall")) {
+			
+			if(!existsParam("application") || !existsParam("data") ) {
+				throw new CommandException("MUST specify application and data for a sendall command");
+			}
+			
+			//Which application
+			String application 	= getParam("application");
+
+			//What data
+			MiniData mdata 	= null;
+			
+			if(isParamJSONObject("data")) {
+				MiniString datastr = new MiniString(getJSONObjectParam("data").toString());
+				mdata = new MiniData(datastr.getData());
+			}else {
+				
+				if(!getParam("data").startsWith("0x")) {
+				
+					String text 	= getParam("data");
+					MiniString str 	= new MiniString(text);
+					mdata 			= new MiniData(str.getData());
+					
+				}else {
+					mdata = getDataParam("data");
+				}
+			} 
+			
+			//Now send to all of them - poll message
+			ArrayList<MaximaContact> contacts = maxdb.getAllContacts();
+			for(MaximaContact contact : contacts) {
+				
+				//The actual address
+				String fullto = contact.getCurrentAddress();
+			
+				//Now convert into the correct message..
+				Message sender = createSendMessage(fullto, application, mdata);
+				
+				//Add it to our Polling list of sends..
+				max.PostMessage(sender);
+			}
+			
+			ret.put("response", "Message added to send stack for all contacts");
+			
 		}else {
 			throw new CommandException("Unknown Action : "+func);
 		}
