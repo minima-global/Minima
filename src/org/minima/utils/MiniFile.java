bbc6392dae059ee186a0ee9c1a7500c799d80fee
spartacusrex <patrickcerri@gmail.com>
2023-06-20 14:31:58 +0100

    Added GZIP compress / uncompress functions

--- src/org/minima/utils/MiniFile.java
+++ src/org/minima/utils/MiniFile.java
@@ -13,6 +13,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 import org.minima.objects.base.MiniData;
 import org.minima.system.params.GeneralParams;
@@ -375,4 +377,43 @@ public class MiniFile {
             
         return outputStream.toByteArray();
 	}
+	
+	public static void decompressGzipFile(File gzipFile, File newFile) {
+        try {
+            FileInputStream fis 	= new FileInputStream(gzipFile);
+            GZIPInputStream gis 	= new GZIPInputStream(fis);
+            FileOutputStream fos 	= new FileOutputStream(newFile);
+            byte[] buffer = new byte[1024];
+            int len;
+            while((len = gis.read(buffer)) != -1){
+                fos.write(buffer, 0, len);
+            }
+            //close resources
+            fos.close();
+            gis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+	public static void compressGzipFile(File file, File gzipFile) {
+        try {
+            FileInputStream fis 	= new FileInputStream(file);
+            FileOutputStream fos 	= new FileOutputStream(gzipFile);
+            GZIPOutputStream gzipOS = new GZIPOutputStream(fos);
+            byte[] buffer = new byte[1024];
+            int len;
+            while((len=fis.read(buffer)) != -1){
+                gzipOS.write(buffer, 0, len);
+            }
+            //close resources
+            gzipOS.close();
+            fos.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
 }
