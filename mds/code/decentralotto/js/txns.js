2b2abf87103d05bcc462c19fff0700ee2351622c
spartacusrex <patrickcerri@gmail.com>
2023-07-05 11:48:57 +0100

    lotto 0.3

--- mds/code/decentralotto/js/txns.js
+++ mds/code/decentralotto/js/txns.js
@@ -6,8 +6,8 @@ var LOTTERY_ADVERT_SCRIPT  = "LET lotto=[ This is a DecentraLotto Advert ] LET p
 var LOTTERY_ADVERT_ADDRESS = "MxG081TA19N7Y8G4BGF30BD34GCS77KCKKD43JNUNSYN84T107V1DW78AQDGRY8"
 var LOTTERY_ADVERT_AMOUNT  = "0.00000000000000000001";
 
-var LOTTERY_GAME_SCRIPT 	= "LET round=STATE(0) LET prevround=PREVSTATE(0) ASSERT round EQ INC(prevround) LET playerpubkey=PREVSTATE(1) LET secret=PREVSTATE(2) LET odds=PREVSTATE(3) ASSERT (odds GT 0) AND (odds LT 1) LET lottopubkey=PREVSTATE(4) LET lottoaddress=PREVSTATE(5) IF round EQ 1 THEN IF @COINAGE GT 20 AND SIGNEDBY(playerpubkey) THEN RETURN TRUE ENDIF ASSERT SIGNEDBY(lottopubkey) ASSERT SAMESTATE(1 6) LET lottorand=STATE(7) ASSERT ((HEX(lottorand) EQ lottorand) AND (LEN(lottorand) LTE 32)) LET requiredamount=@AMOUNT/odds RETURN VERIFYOUT(@INPUT @ADDRESS requiredamount @TOKENID TRUE) ELSEIF round EQ 2 THEN IF @COINAGE GT 64 AND SIGNEDBY(lottopubkey) THEN RETURN TRUE ENDIF ASSERT SIGNEDBY(playerpubkey) LET preimage=STATE(8) LET checkhash=SHA3(preimage) ASSERT checkhash EQ secret LET lottorand=PREVSTATE(7) LET decider=SHA3(CONCAT(preimage lottorand)) LET hexsubset=SUBSET(0 8 decider) LET numvalue=NUMBER(hexsubset) LET maxvalue=NUMBER(0xFFFFFFFFFFFFFFFF) LET target=FLOOR(maxvalue*odds) LET iswin=numvalue LTE target IF iswin THEN RETURN TRUE ELSE RETURN VERIFYOUT(@INPUT lottoaddress @AMOUNT @TOKENID TRUE) ENDIF ENDIF";
-var LOTTERY_GAME_ADDRESS 	= "MxG0847MEBNCUHTDGK0FR4HWDZTWQ1KJCNMNHJY56DH15M12403W2WVJU0N39JR";
+var LOTTERY_GAME_SCRIPT 	= "LET round=STATE(0) LET prevround=PREVSTATE(0) ASSERT round EQ INC(prevround) LET playerpubkey=PREVSTATE(1) LET secret=PREVSTATE(2) LET odds=PREVSTATE(3) ASSERT (odds GT 0) AND (odds LT 1) LET lottopubkey=PREVSTATE(4) LET lottoaddress=PREVSTATE(5) IF round EQ 1 THEN IF @COINAGE GT 20 AND SIGNEDBY(playerpubkey) THEN RETURN TRUE ENDIF ASSERT SIGNEDBY(lottopubkey) ASSERT SAMESTATE(1 6) LET lottorand=STATE(7) ASSERT ((HEX(lottorand) EQ lottorand) AND (LEN(lottorand) LTE 32)) LET requiredamount=@AMOUNT/odds RETURN VERIFYOUT(@INPUT @ADDRESS requiredamount @TOKENID TRUE) ELSEIF round EQ 2 THEN IF @COINAGE GT 20 AND SIGNEDBY(lottopubkey) THEN RETURN TRUE ENDIF ASSERT SIGNEDBY(playerpubkey) LET preimage=STATE(8) LET checkhash=SHA3(preimage) ASSERT checkhash EQ secret LET lottorand=PREVSTATE(7) LET decider=SHA3(CONCAT(preimage lottorand)) LET hexsubset=SUBSET(0 8 decider) LET numvalue=NUMBER(hexsubset) LET maxvalue=NUMBER(0xFFFFFFFFFFFFFFFF) LET target=FLOOR(maxvalue*odds) LET iswin=numvalue LTE target IF iswin THEN RETURN TRUE ELSE RETURN VERIFYOUT(@INPUT lottoaddress @AMOUNT @TOKENID TRUE) ENDIF ENDIF";
+var LOTTERY_GAME_ADDRESS 	= "MxG08743CMGQZ27HWANK8VJJZF44689YG3V0H0STTPBYA0MEYYTH4MADY0FWBNQ";
 
 function addLotteryAdvertAddress(callback){
 	MDS.cmd("newscript trackall:false script:\""+LOTTERY_ADVERT_SCRIPT+"\"", function(resp){
@@ -247,7 +247,7 @@ function checkRoundOneGame(blocknum, coin){
 	
 	//The 2 players
 	var playerpubkey 	= coin.state[1];
-	var lottopubkey 	= coin.state[1];
+	var lottopubkey 	= coin.state[4];
 	
 	//How old is this round
 	var age  			= blocknum - Number(coin.created); 
@@ -271,7 +271,7 @@ function checkRoundOneGame(blocknum, coin){
 		if(false && isplayer && age>20){
 			
 			//It's one of ours.. as PLAYER.. collect it..
-			collectRoundOne(coin,address,playerpubkey);
+			collectExpiredGame(1, coin, address, playerpubkey);
 				
 		}else if(islotto){
 			
@@ -281,7 +281,7 @@ function checkRoundOneGame(blocknum, coin){
 	}); 
 }
 
-function collectRoundOne(coin,address,pubkey){
+function collectExpiredGame(round, coin, address, pubkey){
 	
 	var txnname="collect_"+coin.coinid;
 	
@@ -292,101 +292,151 @@ function collectRoundOne(coin,address,pubkey){
 	creator		+= ";txnoutput id:"+txnname+" amount:"+coin.amount+" address:"+address;
 	
 	//Set the round state var..
-	creator += ";txnstate id:"+txnname+" port:0 value:1";
+	creator += ";txnstate id:"+txnname+" port:0 value:"+round;
 	
 	//Sign and post and delete
 	creator		+= ";txnsign id:"+txnname+" publickey:"+pubkey;
 	creator		+= ";txnpost id:"+txnname+" auto:true";
 	creator		+= ";txndelete id:"+txnname;
 	
-	MDS.log("COLLECT ROUND ONE AS PLAYER!");
+	MDS.log("COLLECT EXPIRED COIN!");
 	MDS.cmd(creator,function(resp){
 		//MDS.log(JSON.stringify(resp));
 	});
 }
 
-function lottoRoundOne(coin, pubkey, random){
+function lottoRoundOne(coin, random){
 	
 	//Check details are valid - odds min max..
 	var uid = coin.state[6];
 	
-	//Load thislottery
-	loadLottery(uid,function(sqlres){
-		//MDS.log("LOTTERY:"+JSON.stringify(rows));	
-		if(sqlres.count==1){
-			
-			//Get the data
-			var row = sqlres.rows[0]; 
-			
-			//Found the game.. check the values..
-			var coinamount 	= Number(coin.amount);
-			var coinodds 	= Number(coin.state[3]);
-			
-			var min	 = Number(row.MIN);
-			var max	 = Number(row.MAX); 
-			var odds = Number(row.ODDS);
-			
-			var valid = true;
-			if(coinodds != odds){
-				valid = false;
-			}else if(coinamount<min || coinamount>max){
-				valid = false;
-			}
-			
-			//Is it a valid game..
-			if(!valid){
-				MDS.log("[!] INVALID GAME COIN FOUND "+JSON.stringify(row)+" "+JSON.stringify(coin));
-			}else{
-				
-				//Lets play!
-				MDS.log("LETS PLAY! amount:"+coinamount+" odds:"+odds);
-
-				var requiredamount 	= coinamount / odds;
-				var difference 		= requiredamount - coinamount;  
-		
-				var txnname="lotto1_"+coin.coinid;
+	//The pubkey and address
+	var lottopubkey  = coin.state[4];
+	var lottoaddress = coin.state[5];
 	
-				var creator  = "";
-				creator		+= "txndelete id:"+txnname;
-				creator		+= ";txncreate id:"+txnname;
+	//Check the address
+	MDS.cmd("checkaddress address:"+lottoaddress,function(resp){
+		if(resp.status){
+			if(resp.response.simple){
+				//It's one ot yours
+				loadLottery(uid,function(sqlres){
+					//MDS.log("LOTTERY:"+JSON.stringify(rows));	
+					if(sqlres.count==1){
+						
+						//Get the data
+						var row = sqlres.rows[0]; 
+						
+						//Found the game.. check the values..
+						var coinamount 	= Number(coin.amount);
+						var coinodds 	= Number(coin.state[3]);
+						
+						var min	 = Number(row.MIN);
+						var max	 = Number(row.MAX); 
+						var odds = Number(row.ODDS);
+						
+						var valid = true;
+						if(coinodds != odds){
+							valid = false;
+						}else if(coinamount<min || coinamount>max){
+							valid = false;
+						}
+						
+						//CHECK ADDRESS AND PUBKEY!!
+						//..
+						
+						//Is it a valid game..
+						if(!valid){
+							MDS.log("[!] INVALID GAME COIN FOUND "+JSON.stringify(row)+" "+JSON.stringify(coin));
+						}else{
+							
+							//Lets play!
+							MDS.log("LETS PLAY! amount:"+coinamount+" odds:"+odds+" lottopubkey:"+pubkey);
+			
+							var requiredamount 	= coinamount / odds;
+							var difference 		= requiredamount - coinamount;  
+					
+							var txnname="lotto1_"+coin.coinid;
 				
-				creator		+= ";txninput id:"+txnname+" coinid:"+coin.coinid;
-				creator		+= ";txnoutput id:"+txnname+" amount:"+requiredamount+" address:"+coin.address;
-				
-				//Set the round state var
-				creator += ";txnstate id:"+txnname+" port:0 value:1";
-				
-				//Add all the current state vars.. except state
-				for(var st=1;st<7;st++){
-					creator += ";txnstate id:"+txnname+" port:"+st+" value:"+coin.state[st];	
-				}
-				
-				//And now add our OWN random number
-				creator += ";txnstate id:"+txnname+" port:7 value:"+random;
-				
-				//NOW - add the difference of the amount.. ONLY the change sent back
-				creator += ";txnaddamount id:"+txnname+" amount:"+difference+" onlychange:true";
-				
-				//Now sign automagically all the added coin from txnaddamount
-				creator		+= ";txnsign id:"+txnname+" publickey:auto";
-				
-				//Now SIGN with the LOTTO pubkey
-				creator		+= ";txnsign id:"+txnname+" publickey:"+pubkey;
-				
-				//Post and delete
-				creator		+= ";txnpost id:"+txnname+" auto:true";
-				creator		+= ";txndelete id:"+txnname;
-				
-				MDS.cmd(creator,function(resp){
-					MDS.log("BASE CREATED!");
-				});
+							var creator  = "";
+							creator		+= "txndelete id:"+txnname;
+							creator		+= ";txncreate id:"+txnname;
+							
+							creator		+= ";txninput id:"+txnname+" coinid:"+coin.coinid;
+							creator		+= ";txnoutput id:"+txnname+" amount:"+requiredamount+" address:"+coin.address;
+							
+							//Set the round state var
+							creator += ";txnstate id:"+txnname+" port:0 value:1";
+							
+							//Add all the current state vars.. except state
+							for(var st=1;st<7;st++){
+								creator += ";txnstate id:"+txnname+" port:"+st+" value:"+coin.state[st];	
+							}
+							
+							//And now add our OWN random number
+							creator += ";txnstate id:"+txnname+" port:7 value:"+random;
+							
+							//NOW - add the difference of the amount.. ONLY the change sent back
+							creator += ";txnaddamount id:"+txnname+" amount:"+difference+" onlychange:true";
+							
+							//Now sign automagically all the added coin from txnaddamount
+							creator		+= ";txnsign id:"+txnname+" publickey:auto";
+							
+							//Now SIGN with the LOTTO pubkey
+							creator		+= ";txnsign id:"+txnname+" publickey:"+lottopubkey;
+							
+							//Post and delete
+							creator		+= ";txnpost id:"+txnname+" auto:true";
+							creator		+= ";txndelete id:"+txnname;
+							
+							MDS.cmd(creator,function(resp){
+								MDS.log("BASE CREATED!");
+							});
+						}
+					}else{
+						MDS.log("GAME NOT FOUND! uid:"+uid);
+					}
+				});			
 			}
-		}else{
-			MDS.log("GAME NOT FOUND! uid:"+uid);
 		}
 	});	
 }
 
 function checkRoundTwoGame(blocknum, coin){
+	
+	//The 2 players
+	var playerpubkey 	= coin.state[1];
+	var lottopubkey 	= coin.state[1];
+	
+	//How old is this round
+	var age  			= blocknum - Number(coin.created); 
+	
+	//Check this IS actually a PLAYER game
+	var cmdstr 	 = "keys action:list publickey:"+playerpubkey;
+	cmdstr 		+= ";keys action:list publickey:"+lottopubkey;
+	cmdstr 		+= ";getaddress";
+	
+	//Run it..
+	MDS.cmd(cmdstr,function(resp){
+		
+		//Which player are we  - can be both..
+		var isplayer = resp[0].response.total>0;
+		var islotto  = resp[1].response.total>0;
+		var address	 = resp[2].response.address;
+		
+		//Is it one of ours..
+		if(islotto && age>64){
+			
+			//It's one of ours.. as PLAYER.. collect it..
+			collectExpiredGame(2,coin,address,lottopubkey);
+				
+		}else if(isplayer){
+			
+			//We are the lotto..
+			//playerRoundTwo(coin,playerpubkey);
+		}
+	});
+}
+
+function playerRoundTwo(coin,pubkey){
 	
 }
