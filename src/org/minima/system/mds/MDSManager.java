b6fb3b3e6af0afd89b949bbb1c72ea288f4968f2
spartacusrex <patrickcerri@gmail.com>
2023-08-09 09:23:29 +0100

    new datpps

--- src/org/minima/system/mds/MDSManager.java
+++ src/org/minima/system/mds/MDSManager.java
@@ -860,27 +860,27 @@ public class MDSManager extends MessageProcessor {
 			checkInstalled("pending", "default/pending-1.0.5.mds.zip", allminis, true);
 			
 			//Security MiniDAPP - backups / restore
-			checkInstalled("security", "default/security-0.16.1.mds.zip", allminis, true);
+			checkInstalled("security", "default/security-0.17.2.mds.zip", allminis, true);
 			
 			//Dappstore gets write permissions
-			checkInstalled("dapp store", "default/dapp_store-1.0.4.mds.zip", allminis, true);
+			checkInstalled("dapp store", "default/dapp_store-1.0.5.mds.zip", allminis, true);
 			
 			//The rest are normal
 			checkInstalled("block", "default/block-2.1.1.mds.zip", allminis, false);
-			checkInstalled("chatter", "default/chatter-1.5.2.mds.zip", allminis, false);
+			checkInstalled("chatter", "default/chatter-1.6.0.mds.zip", allminis, false);
 			checkInstalled("docs", "default/docs-1.4.0.mds.zip", allminis, false);
-			checkInstalled("filez", "default/filez-1.9.1.mds.zip", allminis, false);
+			checkInstalled("filez", "default/filez-1.9.2.mds.zip", allminis, false);
 			checkInstalled("future cash", "default/futurecash-1.11.1.mds.zip", allminis, false);
 			checkInstalled("health", "default/health-1.1.3.mds.zip", allminis, false);
 			checkInstalled("logs", "default/logs-1.0.1.mds.zip", allminis, false);
-			checkInstalled("maxcontacts", "default/maxcontacts-1.6.1.mds.zip", allminis, false);
+			checkInstalled("maxcontacts", "default/maxcontacts-1.7.1.mds.zip", allminis, false);
 			checkInstalled("maxsolo", "default/maxsolo-2.4.4.mds.zip", allminis, false);
 			checkInstalled("news feed", "default/news-2.0.mds.zip", allminis, false);
 			checkInstalled("script ide", "default/scriptide-2.0.1.mds.zip", allminis, false);
 			checkInstalled("sql bench", "default/sqlbench-0.4.mds.zip", allminis, false);
 			checkInstalled("terminal", "default/terminal-2.3.0.mds.zip", allminis, false);
 			checkInstalled("vestr", "default/vestr-1.4.4.mds.zip", allminis, false);
-			checkInstalled("wallet", "default/wallet-2.24.3.mds.zip", allminis, false);
+			checkInstalled("wallet", "default/wallet-2.25.0.mds.zip", allminis, false);
 		}
 	}
 	
