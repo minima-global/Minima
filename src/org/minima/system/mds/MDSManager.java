b77e272f8670e5d599fd728cfa066128721195a7
spartacusrex <patrickcerri@gmail.com>
2023-08-11 09:20:29 +0100

    latest minihub

--- src/org/minima/system/mds/MDSManager.java
+++ src/org/minima/system/mds/MDSManager.java
@@ -854,7 +854,7 @@ public class MDSManager extends MessageProcessor {
 		ArrayList<MiniDAPP> allminis = mdb.getAllMiniDAPPs();
 				
 		//Check for HUB
-		checkInstalled("minihub", "minihub/minihub-0.10.2.mds.zip", allminis, true, true);
+		checkInstalled("minihub", "minihub/minihub-0.11.0.mds.zip", allminis, true, true);
 		
 		//Do we Install the Default MiniDAPPs
 		if(GeneralParams.DEFAULT_MINIDAPPS) {
