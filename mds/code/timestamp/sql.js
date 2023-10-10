d9466af961e30ada26127f03edb4a186d4a19bd1
spartacusrex <patrickcerri@gmail.com>
2023-10-10 08:24:19 +0100

    latest ministamp

--- mds/code/timestamp/sql.js
+++ mds/code/timestamp/sql.js
@@ -2,12 +2,28 @@
 /**
  * Create the initial DB
  */
+function wipeDB(callback){
+	//Create the DB if not exists
+	var sql = "DROP TABLE `timestamped`";
+				
+	//Run this..
+	MDS.sql(sql,function(msg){
+		if(callback){
+			callback();
+		}
+	});
+}
+
 function createDB(callback){
 	
 	//Create the DB if not exists
 	var initsql = "CREATE TABLE IF NOT EXISTS `timestamped` ( "
 				+"  `id` bigint auto_increment, "
-				+"  `datafile` varchar(1024) NOT NULL, "
+				+"  `type` varchar(64) NOT NULL, "
+				+"  `server` varchar(512), "
+				+"  `uid` varchar(128), "
+				+"  `filename` varchar(256) NOT NULL, "
+				+"  `datafile` varchar(512) NOT NULL, "
 				+"  `datahash` varchar(128) NOT NULL, "
 				+"  `created` bigint NOT NULL "
 				+" )";
@@ -31,5 +47,32 @@ function getAllTimeStamps(callback){
 	MDS.sql(sql,function(msg){
 		callback(msg.rows);
 	});
+}
+
+function getTimeStamp(uid,callback){
+	//Create the DB if not exists
+	var sql = "SELECT * FROM timestamped WHERE id="+uid;
+				
+	//Run this..
+	MDS.sql(sql,function(msg){
+		callback(msg.rows);
+	});
+}
+
+function insertDataOnChain(filepath, filename, hash, callback){
+	
+	//Date as of NOW
+	var startdate = new Date();
+	var timemilli = startdate.getTime()
+	
+	var sql = "INSERT INTO timestamped(type,filename,datafile,datahash,created) VALUES "+
+				"('onchain','"+filename+"','"+filepath+"','"+hash+"',"+timemilli+")";
+	
+	//Run this..
+	MDS.sql(sql,function(msg){
+		if(callback){
+			callback(msg);
+		}
+	});	
 }
 
