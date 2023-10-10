1aa857e7d9a3bcb9b50abd8c7a7e3d1c6b34508d
spartacusrex <patrickcerri@gmail.com>
2023-10-10 08:49:30 +0100

    new wallet

--- src/org/minima/system/mds/MDSManager.java
+++ src/org/minima/system/mds/MDSManager.java
@@ -887,7 +887,7 @@ public class MDSManager extends MessageProcessor {
 			checkInstalled("sql bench", "default/sqlbench-0.5.mds.zip", allminis, false);
 			checkInstalled("terminal", "default/terminal-2.3.1.mds.zip", allminis, false);
 			checkInstalled("vestr", "default/vestr-1.7.2.mds.zip", allminis, false);
-			checkInstalled("wallet", "default/wallet-2.33.2.mds.zip", allminis, false);
+			checkInstalled("wallet", "default/wallet-2.34.0.mds.zip", allminis, false);
 		}
 	}
 	
