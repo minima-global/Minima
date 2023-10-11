d07be3e31d3a50f2195eea5ffa0550a36249e195
spartacusrex <patrickcerri@gmail.com>
2023-10-11 08:42:25 +0100

    latest timestamp minidapp

--- mds/code/timestamp/sql.js
+++ mds/code/timestamp/sql.js
@@ -22,6 +22,10 @@ function createDB(callback){
 				+"  `type` varchar(64) NOT NULL, "
 				+"  `server` varchar(512), "
 				+"  `uid` varchar(128), "
+				+"  `serverstatus` int DEFAULT 0, "
+				+"  `roothash` varchar(128), "
+				+"  `proof` varchar(1024), "
+				+"  `address` varchar(128), "
 				+"  `filename` varchar(256) NOT NULL, "
 				+"  `datafile` varchar(512) NOT NULL, "
 				+"  `datahash` varchar(128) NOT NULL, "
@@ -67,6 +71,35 @@ function insertDataOnChain(filepath, filename, hash, callback){
 	
 	var sql = "INSERT INTO timestamped(type,filename,datafile,datahash,created) VALUES "+
 				"('onchain','"+filename+"','"+filepath+"','"+hash+"',"+timemilli+")";
+	
+	//Run this..
+	MDS.sql(sql,function(msg){
+		if(callback){
+			callback(msg);
+		}
+	});	
+}
+
+function insertDataServer(filepath, filename, hash, server, uid, callback){
+	
+	//Date as of NOW
+	var startdate = new Date();
+	var timemilli = startdate.getTime()
+	
+	var sql = "INSERT INTO timestamped(type,server,uid,filename,datafile,datahash,created) VALUES "+
+				"('server','"+server+"','"+uid+"','"+filename+"','"+filepath+"','"+hash+"',"+timemilli+")";
+	
+	//Run this..
+	MDS.sql(sql,function(msg){
+		if(callback){
+			callback(msg);
+		}
+	});	
+}
+
+function updateDataServer(uid, root, proof, address, callback){
+	
+	var sql = "UPDATE timestamped SET serverstatus=1, address='"+address+"', roothash='"+root+"', proof='"+proof+"' WHERE uid='"+uid+"'";
 	
 	//Run this..
 	MDS.sql(sql,function(msg){
