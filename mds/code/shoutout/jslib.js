07647ce68a340e641f3980afa7260ba78cf5922b
spartacusrex <patrickcerri@gmail.com>
2023-10-16 15:28:31 +0100

    latest shoutout

--- mds/code/shoutout/jslib.js
+++ mds/code/shoutout/jslib.js
@@ -1,9 +1,24 @@
 
-//const VALID_CATEGORY_REGEX = new RegExp("^[\p{L}0-9.]*$");
-//const VALID_CATEGORY_REGEX = new XRegExp("^[\\p{L}0-9._]*$");
 const VALID_CATEGORY_REGEX = new XRegExp("^[\\p{L}\\p{N}._]*$");
 
-//const VALID_CATEGORY_REGEX = new XRegExp("^[a-z0-9.]*$");
+function cleanCategory(category){
+	
+	//Remove spaces
+	var newcat = category.replaceAll(" ","");
+	while(newcat.indexOf("..") != -1){
+		newcat = newcat.replaceAll("..",".");
+	}
+	
+	if(newcat.startsWith(".")){
+		newcat = newcat.substring(1);
+	}
+	
+	if(newcat.endsWith(".")){
+		newcat = newcat.substring(0,newcat.length-1);
+	}
+	
+	return newcat;
+}
 
 function checkCategory(category){
 	var valid = VALID_CATEGORY_REGEX.test(category);
