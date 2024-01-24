3e944ebcf26b55f3e5e00bb6bfca32b6e16bd5e8
spartacusrex <patrickcerri@gmail.com>
2024-01-24 12:57:37 +0000

    order book teck works

---
+++ mds/code/nftmarket/service.js
@@ -0,0 +1,2 @@
+
+//MDS.load("ethers-5.2.umd.min.js");
---
+++ mds/code/paychecker/jslib.js
@@ -0,0 +1,21 @@
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
