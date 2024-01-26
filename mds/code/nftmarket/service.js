1f27b967033b5d14f08edab224fefe9326c620a4
spartacusrex <patrickcerri@gmail.com>
2024-01-26 10:04:40 +0000

    latest eth tests

--- mds/code/nftmarket/service.js
+++ mds/code/nftmarket/service.js
@@ -1,2 +1,37 @@
 
+
+
 //MDS.load("ethers-5.2.umd.min.js");
+/*
+MDS.load("ethers-v4.min.js");
+
+//let provider = ethers.getDefaultProvider();
+
+MDS.log("Start create Wallet..");
+
+let privateKey = "0xC8E9BBBA0CFC3E54E381DD1DDA4A1FE7D29AF501CE418B9C7415FC91E2B34F64";
+
+let wallet = new ethers.Wallet(privateKey);
+
+MDS.log("Wallet created! "+wallet.address);
+
+let txParams = {
+  nonce: '0x00',
+  gasPrice: '0x09184e72a000',
+  gasLimit: '0x2710',
+  to: '0x0000000000000000000000000000000000000000',
+  value: '0x00',
+  data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057',
+}
+
+MDS.log("Now sign..");
+
+let signPromise = wallet.sign(txParams);
+
+MDS.log("Promise returned..");
+
+signPromise.then((signedTransaction) => {
+    MDS.log(signedTransaction);
+})
+
+*/
