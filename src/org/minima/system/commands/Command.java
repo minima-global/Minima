512f53a97351a9acc312f5ae4affc01ca2e62332
spartacusrex <patrickcerri@gmail.com>
2024-02-19 10:38:22 +0000

    add quit as write command

--- src/org/minima/system/commands/Command.java
+++ src/org/minima/system/commands/Command.java
@@ -775,7 +775,7 @@ public abstract class Command {
 	private static final String[] ALL_WRITE_COMMANDS = 
 		{"send","sendpoll","sendsign","multisig","tokencreate","consolidate",
 		 "cointrack","sign","txnsign","mds","backup","removescript",
-		 "restore","restoresync","vault","archive","mysql","rpc","magic"};
+		 "restore","restoresync","vault","archive","mysql","rpc","magic","quit"};
 	
 	private static final ArrayList<String> ALL_WRITE_COMMANDS_ARRAY = new ArrayList<String>(Arrays.asList(ALL_WRITE_COMMANDS));
 	
