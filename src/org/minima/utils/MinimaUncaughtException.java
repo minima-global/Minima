24bef960f64117e0f347bb10dd26647fa15a2cb4
spartacusrex <patrickcerri@gmail.com>
2024-09-26 10:22:25 +0100

    catch timeout exceptions

--- src/org/minima/utils/MinimaUncaughtException.java
+++ src/org/minima/utils/MinimaUncaughtException.java
@@ -20,11 +20,27 @@ public class MinimaUncaughtException implements UncaughtExceptionHandler {
 		//What type of error is it!
 		if(zThrowable instanceof java.lang.OutOfMemoryError) {
 			MinimaLogger.log("[!] MEMORY ERROR.. SHUTTING DOWN");
+			
 			Runtime.getRuntime().halt(0);
+			//System.exit(0);
+			
+			return;
 			
 		}else if(zThrowable instanceof org.h2.mvstore.MVStoreException) {
 			MinimaLogger.log("[!] H2 DATABASE MVStoreException.. SHUTTING DOWN");
+			
 			Runtime.getRuntime().halt(0);
+			//System.exit(0);
+			
+			return;
+		
+		}else if(zThrowable instanceof java.util.concurrent.TimeoutException) {
+			MinimaLogger.log("[!] Concurrent Timeout Exception.. SHUTTING DOWN");
+			
+			Runtime.getRuntime().halt(0);
+			//System.exit(0);
+			
+			return;
 		}
 		
 		//Otherwise pass to the default..
