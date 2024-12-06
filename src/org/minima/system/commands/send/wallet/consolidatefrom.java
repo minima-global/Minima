861d77b6850a9d16ecec4828280eb0e412639825
spartacusrex <patrickcerri@gmail.com>
2024-12-06 09:17:50 +0000

    output 10 coins not just 1 on consolidate

--- src/org/minima/system/commands/send/wallet/consolidatefrom.java
+++ src/org/minima/system/commands/send/wallet/consolidatefrom.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.Comparator;
 
 import org.minima.database.MinimaDB;
+import org.minima.database.txpowdb.TxPoWDB;
 import org.minima.database.txpowtree.TxPoWTreeNode;
 import org.minima.objects.Coin;
 import org.minima.objects.base.MiniData;
@@ -64,7 +65,8 @@ public class consolidatefrom extends Command {
 		//Are we mining
 		boolean mine 		= getBooleanParam("mine", true);
 		
-		TxPoWTreeNode tip = MinimaDB.getDB().getTxPoWTree().getTip();
+		TxPoWTreeNode tip 		= MinimaDB.getDB().getTxPoWTree().getTip();
+		MiniNumber currentblock = tip.getBlockNumber(); 
 		
 		//Get all the coins for this addres / token
 		ArrayList<Coin> coins = TxPoWSearcher.searchCoins(	tip, false, 
@@ -85,12 +87,43 @@ public class consolidatefrom extends Command {
 			}
 		});
 		
+		//Now make sure they are old enough and not in mempool..
+		TxPoWDB txpdb 		= MinimaDB.getDB().getTxPoWDB();
+		MiniNumber minage 	= new MiniNumber(3);
+		ArrayList<Coin> validcoins = new ArrayList<>();
+		for(Coin cc : coins){
+			
+			boolean stillgood = true;
+			
+			//Check age..
+			if(currentblock.sub(cc.getBlockCreated()).isLess(minage)) {
+				stillgood = false;
+			}
+			
+			//Check mempool
+			if(stillgood) {
+				if(txpdb.checkMempoolCoins(cc.getCoinID())) {
+					stillgood = false;
+				}
+			}
+			
+			//Are we still good!
+			if(stillgood) {
+				validcoins.add(cc);
+			}
+		}
+		
+		//Are there any valid coins
+		if(validcoins.size() == 0) {
+			throw new CommandException("No valid coins found to consolidate. Coins must be "+minage.toString()+" blocks old and not already in mempool.");
+		}
+		
 		//Now add the top 20 coins
 		int coincount	 = 1;
 		MiniNumber total = MiniNumber.ZERO;
 		
 		//Cycle through the top coins
-		for(Coin cc : coins){
+		for(Coin cc : validcoins){
 			total = total.add(cc.getTokenAmount()); 
 			coincount++;
 			
@@ -117,9 +150,15 @@ public class consolidatefrom extends Command {
 			}
 		}
 		
-		//And now add the output
-		String command 	= "txnoutput id:"+randomid+" amount:"+total+" address:"+fromaddress+" tokenid:"+tokenid;
-		result 			= runCommand(command);
+		//And now add the output - 10 outputs
+		MiniNumber smallout = total.div(new MiniNumber(10));
+		for(int i=0;i<10;i++) {
+			String command 	= "txnoutput id:"+randomid+" amount:"+smallout+" address:"+fromaddress+" tokenid:"+tokenid;
+			result 			= runCommand(command);
+		}
+		
+		//String command 	= "txnoutput id:"+randomid+" amount:"+total+" address:"+fromaddress+" tokenid:"+tokenid;
+		//result 			= runCommand(command);
 		
 		//Add the scripts..
 		runCommand("txnscript id:"+randomid+" scripts:{\""+script+"\":\"\"}");
@@ -140,6 +179,64 @@ public class consolidatefrom extends Command {
 		ret.put("response", result.get("response"));
 		
 		return ret;
+	}
+	
+	public JSONObject createConsolidate(ArrayList<Coin> zAllCoins, MiniNumber zBurn, 
+				String zFromAddress, String zTokenid, String zScript, 
+				String zPrivateKey, MiniNumber zKeyUses) throws CommandException {
+		
+		//ID of the custom transaction
+		String randomid 	= MiniData.getRandomData(32).to0xString();
+				
+		//Now construct the transaction..
+		JSONObject result 	= runCommand("txncreate id:"+randomid);
+		
+		//Now add ALL these coins
+		int coincount	 = 1;
+		MiniNumber total = MiniNumber.ZERO;
+		
+		//Cycle through the top coins
+		for(Coin cc : zAllCoins){
+			total = total.add(cc.getTokenAmount()); 
+			
+			//Add this coin to the transaction
+			runCommand("txninput id:"+randomid+" coinid:"+cc.getCoinID().to0xString());
+		}
+		
+		//Now do the burn..
+		if(zBurn.isMore(MiniNumber.ZERO)) {
+			if(zBurn.isLess(total)) {
+				total = total.sub(zBurn);
+			}else {
+				
+				//Remove the txn..
+				runCommand("txndelete id:"+randomid);
+				
+				throw new CommandException("Burn greater than total amount added "+zBurn.toString()+" / "+total);
+			}
+		}
+		
+		//And now add the output
+		String command 	= "txnoutput id:"+randomid+" amount:"+total+" address:"+zFromAddress+" tokenid:"+zTokenid;
+		result 			= runCommand(command);
+		
+		//Add the scripts..
+		runCommand("txnscript id:"+randomid+" scripts:{\""+zScript+"\":\"\"}");
+		
+		//Sort the MMR
+		runCommand("txnmmr id:"+randomid);
+		
+		//Now SIGN
+		runCommand("txnsign id:"+randomid+" publickey:custom privatekey:"+zPrivateKey+" keyuses:"+zKeyUses.toString());
+		
+		//And POST!
+		result = runCommand("txnpost id:"+randomid+" mine:true");
+		
+		//And delete..
+		runCommand("txndelete id:"+randomid);
+		
+		//Return the POST response
+		return (JSONObject)result.get("response");
 	}
 	
 	public JSONObject runCommand(String zCommand) {
