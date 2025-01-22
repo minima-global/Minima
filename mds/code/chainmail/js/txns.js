263717a4eec9226f92d85bdc1b3f3c9058f4ce85
patrickcerri <patrickcerri@gmail.com>
2025-01-22 09:59:50 +0000

    encode decode messages

--- mds/code/chainmail/js/txns.js
+++ mds/code/chainmail/js/txns.js
@@ -2,63 +2,103 @@
 //BASIC ADDRESS
 var CHAINMAIL_ADDRESS = "0x434841494E4D41494C";
 
+function URLencodeString(str){
+	return encodeURIComponent(str).split("'").join("%27");
+}
+
+function URLdecodeString(str){
+	return decodeURIComponent(str).split("%27").join("'");
+}
+
 //Send a file packet..
 function sendMessage(sendjson, callback){
 	
-	//Who is this message for
-	var pubkeyto = sendjson.to;
-	
-	//First convert the JSON - from sql.js
-	var strversion = encodeStringForDB(JSON.stringify(sendjson));
-	
-	//Now do a maxmessage!
-	MDS.cmd("convert from:string to:hex data:\""+strversion+"\"", function(resp){
-		//console.log(JSON.stringify(resp,null,2));
+	try{
 		
-		//The HEX data to encrypt
-		var hexdata = resp.response.conversion;
+		//Who is this message for
+		var pubkeyto = sendjson.to;
 		
-		//Now convert to an encrypted max message..
-		MDS.cmd("maxmessage action:encrypt publickey:"+pubkeyto+" data:"+hexdata, function(maxmess){
+		//First convert the JSON - from sql.js
+		var strversion = URLencodeString(JSON.stringify(sendjson));
+		
+		//Now do a maxmessage!
+		MDS.cmd("convert from:string to:hex data:\""+strversion+"\"", function(resp){
 			
-			//OK - Now send this data on a txn..
-			var state = {};
-			state[99]  = maxmess.response.data;
+			//The HEX data to encrypt
+			var hexdata = resp.response.conversion;
 			
-			//Now construct a txn
-			var txn = "send amount:0.000000000001 address:"+CHAINMAIL_ADDRESS+" state:"+JSON.stringify(state);
-			
-			//Now post..
-			MDS.cmd(txn,function(resp){
-				if(callback){
-					callback(resp);	
+			//Now convert to an encrypted max message..
+			MDS.cmd("maxmessage action:encrypt publickey:"+pubkeyto+" data:"+hexdata, function(maxmess){
+				
+				//Did we encrypt it..
+				if(!maxmess.status){
+					callback(false,"Invalid Public Key");
+					return;
 				}
-			});		
-		});
-	});
+				
+				//OK - Now send this data on a txn..
+				var state = {};
+				state[99]  = maxmess.response.data;
+				
+				//Now construct a txn
+				var txn = "sendpoll amount:0.000000000001 address:"+CHAINMAIL_ADDRESS+" state:"+JSON.stringify(state);
+				
+				//Now post..
+				MDS.cmd(txn,function(resp){
+					if(callback){
+						callback(true);	
+					}
+				});		
+			});
+		});	
+		
+	}catch(error){
+		callback(false,error);
+	}
 }
 
 function checkEncryptedData(data, callback){
 	
-	//First try and decode it..
-	MDS.cmd("maxmessage action:decrypt data:"+data,function(decresp){
+	try{
 		
-		//Is this message for us ?
-		if(!decresp.status){
-			callback(false);
-			return;
-		}
+		//First try and decode it..
+		MDS.cmd("maxmessage action:decrypt data:"+data,function(decresp){
+			console.log(JSON.stringify(decresp,null,2));
+			
+			//Is this message for us ?
+			if(!decresp.status){
+				callback(false);
+				return;
+			}
+			
+			//OK - decrypted OK must be for us.. check signature!
+			if(!decresp.response.message.valid){
+				MDS.log("INVALID Signature from message ?");
+				callback(false);
+				return;
+			}
+			
+			//Decode the data into the orginal message
+			var decodeddata = decresp.response.message.data 
+			
+			//Now to convert back into a string...
+			MDS.cmd("convert from:HEX to:string data:"+decodeddata,function(conv){
+				
+				//Now decode back to normal..
+				var normal = URLdecodeString(conv.response.conversion);
+				
+				//And make a JSON..
+				var original = JSON.parse(normal);
+				
+				console.log(JSON.stringify(original,null,2));
+								
+				//And send it back..
+				callback(true,original);		
+			});
+		});	
 		
-		//OK - decrypted OK must be for us.. check signature!
-		if(!decodeddata.message.valid){
-			MDS.log("INVALID Signature from message ?");
-			callback(false);
-			return;
-		}
-		
-		//Decode the data into the orginal message
-		var decodeddata = decresp.message.data 
-		
-	});
-	
+	}catch(error){
+		MDS.log("ERROR decoding.. "+error);
+		callback(false);
+	}
 }
