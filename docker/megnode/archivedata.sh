750ab1a9b786267286297b56c55fdb01ad86252b
patrickcerri <patrickcerri@gmail.com>
2025-03-05 18:30:41 +0000

    daily mysql and megammr backups..

--- docker/megnode/archivedata.sh
+++ docker/megnode/archivedata.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
+#FIRST DO THE MYSQL
 #Create a filename based on the time..
-filename=minima_archive_$(date +"%Y_%m_%d-%T").gzip
-exportfile=/home/minima/backups/$filename
+filename=minima_mysql_$(date +"%Y_%m_%d-%T").raw.dat
+exportfile=/home/minima/backups/mysql/$filename
 
 #Run a command on Minima node..
-curl minima:9005/mysql+file:$exportfile+host:mysql:3306+database:archivedb+user:archiveuser+password:archivepassword+action:h2export 
+curl minima:9005/mysql+file:$exportfile+action:rawexport 
+
+#Delete files that are older than 21 days
+find /home/minima/backups/mysql/ -type f -mtime +21 -delete
+
+#NOW DO THE MEGAMMR
+#Create a filename based on the time..
+filenamemmr=minima_megammr_$(date +"%Y_%m_%d-%T").mmr
+exportfilemmr=/home/minima/backups/megammr/$filenamemmr
+
+#Run a command on Minima node..
+curl minima:9005/megammr+file:$exportfilemmr+action:export 
+
+#Delete files that are older than 21 days
+find /home/minima/backups/megammr/ -type f -mtime +21 -delete
+
