8f4e880f14b01112e8c7676aaea9cb1d0ae3581e
spartacusrex <patrickcerri@gmail.com>
2025-05-15 11:33:27 +0100

    latest

--- mds/code/omnichannel/js/jslib.js
+++ mds/code/omnichannel/js/jslib.js
@@ -34,3 +34,12 @@ function showTitleOnAndroid(){
 		Android.showTitleBar();
 	}
 }
+
+function genRandomHexString(len) {
+    const hex = '0123456789ABCDEF';
+    let output = '';
+    for (let i = 0; i < len; ++i) {
+        output += hex.charAt(Math.floor(Math.random() * hex.length));
+    }
+    return output;
+}
