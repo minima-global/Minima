10880b4034c0c66d5ea8674db0a26f43970d7b07
spartacusrex <patrickcerri@gmail.com>
2025-09-22 12:09:55 +0100

    new linux script to build jars

---
+++ buildjars.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Build the JARS using gradle"
+./gradlew clean build
+
+echo "Now copy to jar folder"
+cp -v ./build/libs/minima.jar ./jar/minima-nolibs.jar
+cp -v ./build/libs/minima-all.jar ./jar/minima.jar
+
+echo Done
+
