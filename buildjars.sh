59f8e7f07ede8f7f87769ae9dedecab5d9f35ba1
spartacusrex <patrickcerri@gmail.com>
2025-09-23 13:14:43 +0100

    list files after build

--- buildjars.sh
+++ buildjars.sh
@@ -7,5 +7,8 @@ echo "Now copy to jar folder"
 cp -v ./build/libs/minima.jar ./jar/minima-nolibs.jar
 cp -v ./build/libs/minima-all.jar ./jar/minima.jar
 
+echo List Files
+ls -ls ./jar
+
 echo Done
 
