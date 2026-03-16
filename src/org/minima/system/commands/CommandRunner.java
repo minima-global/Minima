bac474f0f49b4fd81a55fb1ee366ba79fd98aa5c
spartacusrex <patrickcerri@gmail.com>
2026-03-16 09:16:57 +0000

    add more functions to write only

--- src/org/minima/system/commands/CommandRunner.java
+++ src/org/minima/system/commands/CommandRunner.java
@@ -613,7 +613,8 @@ public class CommandRunner {
 		{"send","sendpoll","sendsign","multisig","tokencreate","consolidate",
 		 "cointrack","sign","txnsign","mds","backup","removescript",
 		 "restore","restoresync","vault","archive","mysql","mysqlcoins",
-		 "rpc","magic","quit","seedrandom","megammrsync"};
+		 "rpc","magic","quit","seedrandom","megammrsync","megammr","decryptbackup",
+		 "reset"};
 	
 	private final ArrayList<String> ALL_WRITE_COMMANDS_ARRAY = new ArrayList<String>(Arrays.asList(ALL_WRITE_COMMANDS));
 	
