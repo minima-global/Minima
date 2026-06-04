ff0eb2e4b8cf8ae3d1849b410184c8cac138c77c
spartacusrex <patrickcerri@gmail.com>
2026-06-04 14:35:32 +0100

    1.0.49.0

--- src/org/minima/system/params/GlobalParams.java
+++ src/org/minima/system/params/GlobalParams.java
@@ -8,13 +8,13 @@ public class GlobalParams {
 	 * Which Version fo Minima are we running
 	 */
 	public static String MINIMA_BASE_VERSION 	= "1.0";
-	public static String MINIMA_BUILD_NUMBER 	= "48";
+	public static String MINIMA_BUILD_NUMBER 	= "49";
 	public static String MINIMA_VERSION 		= MINIMA_BASE_VERSION+"."+MINIMA_BUILD_NUMBER;
 	
 	/**
 	 * The MICRO build number
 	 */
-	public static String MINIMA_MICRO_BUILD		= "7";
+	public static String MINIMA_MICRO_BUILD		= "0";
 	public static String getFullMicroVersion() {
 		return MINIMA_VERSION+"."+MINIMA_MICRO_BUILD;
 	}
